031a3f17bec26c7f161686c315099d8dd43d6c2e
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-04 20:17:47 +0900

    feat: define LinkBadge

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -2,24 +2,24 @@ import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const variant = {
+  default:
+    "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
+  secondary:
+    "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+  destructive:
+    "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+  outline: "text-foreground",
+};
+export type BadgeVariant = keyof typeof variant;
 
 const badgeVariants = cva(
   "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
-    variants: {
-      variant: {
-        default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
-        secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        destructive:
-          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
-        outline: "text-foreground",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
+    variants: { variant },
+    defaultVariants: { variant: "default" },
   }
 );
 
@@ -27,9 +27,30 @@ export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
+export interface LinkBadgeProps
+  extends React.HTMLAttributes<HTMLAnchorElement>,
+    VariantProps<typeof badgeVariants> {
+  href: `/${string}`;
+}
+
 function Badge({ className, variant, ...props }: BadgeProps) {
   return (
     <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+}
+
+export function LinkBadge({
+  href,
+  className,
+  variant,
+  ...props
+}: LinkBadgeProps) {
+  return (
+    <Link
+      href={href}
+      className={cn(badgeVariants({ variant }), className)}
+      {...props}
+    />
   );
 }
 
