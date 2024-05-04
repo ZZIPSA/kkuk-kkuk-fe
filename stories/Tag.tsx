f24453d86734702cd053f4b572509e60322d3279
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-04 21:22:11 +0900

    feat: define Tag

---
+++ stories/Tag.tsx
@@ -0,0 +1,32 @@
+import { BadgeVariant, LinkBadgeProps, LinkBadge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+
+interface TagProps extends LinkBadgeProps {
+  /**
+   * Link contents
+   */
+  label: string;
+  /**
+   * Where the button should link to
+   */
+  href: `/${string}` | "#";
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Tag = ({ label, href, className, ...props }: TagProps) => {
+  return (
+    <LinkBadge
+      className={cn(
+        "px-2 py-1 text-xs text-primary hover:text-white font-extralight bg-primary/50 hover:bg-primary/80 focus:ring-primary",
+        className
+      )}
+      href={href ? href : "#"}
+      {...props}
+    >
+      {"#"}
+      {label}
+    </LinkBadge>
+  );
+};
