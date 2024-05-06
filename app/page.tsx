31d91a6fdd822de06ce2597ee49f4952d4ecaffa
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-06 16:27:31 +0900

    feat: redirect `/` to `/kits`
    #2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      모바일 화면 최적화 필요 레이아웃 사이드 날리기 필요
-    </main>
-  );
+  return redirect("/kits");
 }
