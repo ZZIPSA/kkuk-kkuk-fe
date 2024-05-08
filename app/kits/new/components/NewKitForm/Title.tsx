7c9b91180c4685ef7a16a4fe33fe736cdcc3229b
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-08 12:59:07 +0900

    feat: define Title field for NewKitForm

---
+++ app/kits/new/components/NewKitForm/Title.tsx
@@ -0,0 +1,34 @@
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { BasicInput as Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { FormFields } from "./types";
+
+const Title: FormFields = ({ control }) => (
+  <FormField
+    control={control}
+    name="title"
+    render={({ field, fieldState }) => (
+      <FormItem>
+        <FormLabel>키트 이름</FormLabel>
+        <FormControl>
+          <Input
+            placeholder="키트 이름을 입력해주세요."
+            className={cn({
+              "border-red-500": !!fieldState.error,
+            })}
+            {...field}
+          />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+export default Title;
