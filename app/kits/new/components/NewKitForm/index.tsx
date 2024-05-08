43d2d093795fca427cba0250890e340bda9b12ed
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-08 13:00:09 +0900

    feat: define NewKitForm component

---
+++ app/kits/new/components/NewKitForm/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Form } from "@/components/ui/form";
+import { Button } from "@/stories/Button";
+
+import { FormValues } from "./types";
+import { FormSchema } from "./schema";
+import Stamps from "./Stamps";
+import Title from "./Title";
+import Description from "./Description";
+import Tags from "./Tags";
+
+export default function NewKitForm() {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: FormValues) {
+    // TODO: Handle form submission
+    console.log(data);
+  }
+  const stampsRef = form.register("stamps");
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <Stamps control={form.control} stampsRef={stampsRef} />
+        <Title control={form.control} />
+        <Tags control={form.control} />
+        <Description control={form.control} />
+        <Button label={"Submit"} type="submit" />
+      </form>
+    </Form>
+  );
+}
