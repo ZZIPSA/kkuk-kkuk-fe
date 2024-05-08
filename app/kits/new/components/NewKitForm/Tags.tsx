3869d0ffefe3d7451c82e9eacad20c8665ad63a3
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-08 12:59:40 +0900

    feat: define Tags dummy field for NewKitForm

---
+++ app/kits/new/components/NewKitForm/Tags.tsx
@@ -0,0 +1,24 @@
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { FormFields } from "./types";
+
+const Tags: FormFields = ({ control }) => (
+  <FormField
+    control={control}
+    name="tags"
+    render={({}) => (
+      <FormItem>
+        <FormLabel>태그</FormLabel>
+        <FormControl>{/* TODO: Tags */}</FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+export default Tags;
