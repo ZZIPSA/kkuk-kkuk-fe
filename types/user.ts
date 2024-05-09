4686a873a2a13e87348ba79ce48da768a5626d5e
Madang Garden <chooi9522@gmail.com>
2024-05-09 23:31:24 +0900

    [feature/#9] 랠리 API 모델 타입, 리스폰스/리퀘스트 타입, 엔드포인트 구현

---
+++ types/user.ts
@@ -0,0 +1,10 @@
+export type User = {
+  id: string;
+  nickname: string;
+  profileImage: string;
+  provider?: string;
+  token?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  deletedAt?: string;
+};
