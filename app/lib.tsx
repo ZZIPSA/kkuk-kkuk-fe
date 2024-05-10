5886f7f129de4c4ef3a3ff7087d86be3e7dd36a1
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-11 04:37:07 +0900

    feat: define navbar items props
    #15

---
+++ app/lib.tsx
@@ -0,0 +1,45 @@
+import { Pencil, Stamp } from "lucide-react";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/stories/Button";
+import { NavBarItemProps } from "./types";
+
+export const staticNavBarItems: NavBarItemProps[] = [
+  {
+    href: "/",
+    Inner: (
+      <Avatar className="w-7 h-7">
+        <AvatarImage src="/kkuk-kkuk.svg" />
+      </Avatar>
+    ),
+  },
+];
+
+export const getSignedNavBarItems = ({
+  image = "/kkuk-kkuk.svg",
+}: {
+  image?: string;
+}): NavBarItemProps[] => [
+  { href: "/kits/new", Inner: <Pencil /> },
+  { href: "/rallies", Inner: <Stamp /> },
+  {
+    href: "/my",
+    Inner: (
+      <Avatar className="w-7 h-7 border">
+        <AvatarImage src={image} />
+      </Avatar>
+    ),
+  },
+];
+
+export const notSignedNavBarItems: NavBarItemProps[] = [
+  {
+    href: "/signin",
+    Inner: (
+      <Button
+        label="로그인"
+        type="button"
+        className="text-sm px-4 h-fit py-1.5 rounded-lg"
+      />
+    ),
+  },
+];
