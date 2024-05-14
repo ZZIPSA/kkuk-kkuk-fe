2320f47935ede65f5db1ac4b715a9b591568462f
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-14 16:06:58 +0900

    issue#8 set line heights as 1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,6 +70,16 @@ const config = {
           to: { height: '0' },
         },
       },
+      fontSize: {
+        xs: ['0.75rem', '1'],
+        sm: ['0.875rem', '1'],
+        base: ['1rem', '1'],
+        lg: ['1.125rem', '1'],
+        xl: ['1.25rem', '1'],
+        '2xl': ['1.5rem', '1'],
+        '3xl': ['1.875rem', '1'],
+        '4xl': ['2.25rem', '1'],
+      },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
