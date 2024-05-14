32280c8fc0a901eec19b0ff15007d6f39231be61
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-15 01:36:21 +0900

    issue#15 set padding for header

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className="border-b">
+        <header className="border-b px-4 py-1">
           <NavBar />
         </header>
         {children}
