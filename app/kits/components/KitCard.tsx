2baa08ac8d1e232ca72bfbde2cce3ce019135bf4
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-14 13:23:54 +0900

    issue#8 get kit type from @type/Kit

--- app/kits/components/KitCard.tsx
+++ app/kits/components/KitCard.tsx
@@ -1,33 +1,20 @@
-import Image from "next/image";
-import Link from "next/link";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import { Tag } from "@/stories/Tag";
-import { KitCardProps } from "../types";
+import Image from 'next/image';
+import Link from 'next/link';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+import { Tag } from '@/stories/Tag';
+import { Kit } from '@/types/kit';
 
-export default function KitCard({
-  kitId,
-  title,
-  description,
-  thumbnail_image,
-  tags,
-  uploader_id,
-  className,
-  ...props
-}: KitCardProps) {
+type KitKeys = 'id' | 'title' | 'description' | 'thumbnailImage' | 'tags' | 'uploaderId';
+type KitCardProps = Pick<Kit, KitKeys> & React.ComponentPropsWithoutRef<typeof Card>;
+
+export default function KitCard({ kitId, title, description, thumbnailImage, tags, uploader: { nickname }, className, ...props }: KitCardProps) {
   return (
-    <Card className={(cn(className), "flex flex-col")} {...props}>
+    <Card className={(cn(className), 'flex flex-col')} {...props}>
       <Link href={`/kits/${kitId}`} passHref>
         <CardHeader>
           <Image
-            src={thumbnail_image}
+            src={thumbnailImage}
             alt={title}
             width={100}
             height={100}
@@ -36,12 +23,8 @@ export default function KitCard({
         </CardHeader>
       </Link>
       <CardContent>
-        <CardTitle className="overflow-hidden whitespace-nowrap overflow-ellipsis">
-          {title}
-        </CardTitle>
-        <CardDescription className="line-clamp-3">
-          {description}
-        </CardDescription>
+        <CardTitle className="overflow-hidden whitespace-nowrap overflow-ellipsis">{title}</CardTitle>
+        <CardDescription className="line-clamp-3">{description}</CardDescription>
       </CardContent>
       <CardFooter className="flex flex-col mt-auto">
         <div className="flex gap-2 overflow-x-auto w-full scrollbar-hide m-4">
@@ -51,15 +34,13 @@ export default function KitCard({
         </div>
         <div className="flex justify-start items-center w-full">
           <Image
-            src={thumbnail_image}
-            alt={uploader_id.toString()}
+            src={thumbnailImage}
+            alt={nickname}
             width={32}
             height={32}
             className="border-gray-400 border-2 rounded-full aspect-square w-8 h-8 object-cover mr-4"
           />
-          <span className="overflow-hidden whitespace-nowrap overflow-ellipsis text-muted-foreground">
-            {uploader_id}
-          </span>
+          <span className="overflow-hidden whitespace-nowrap overflow-ellipsis text-muted-foreground">{nickname}</span>
         </div>
       </CardFooter>
     </Card>
--- app/kits/page.tsx
+++ app/kits/page.tsx
@@ -1,24 +1,14 @@
-import KitCard from "./components/KitCard";
-import { Kit } from "./types";
+import KitCard from './components/KitCard';
+import { Kit } from '@/types/kit';
 
 export default async function KitsPage() {
   const kits: Kit[] = await fetch(process.env.API_URL + "/api/v1/kit")
     .then((res) => res.json());
   return (
     <main className="w-full grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-3 lg:grid-cols-4 lg:gap-4">
-      {kits.map(
-        ({ id, title, description, thumbnail_image, tags, uploader_id }) => (
-          <KitCard
-            kitId={id}
-            key={id}
-            title={title}
-            description={description}
-            thumbnail_image={thumbnail_image}
-            tags={tags}
-            uploader_id={uploader_id}
-          />
-        )
-      )}
+      {kits.map(({ id, title, description, thumbnailImage, tags, uploader }) => (
+        <KitCard id={id} key={id} title={title} description={description} thumbnailImage={thumbnailImage} tags={tags} uploader={uploader} />
+      ))}
     </main>
   );
 }
