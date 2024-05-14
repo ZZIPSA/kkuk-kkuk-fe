13e9a8b5938b7ba4482843dbd395fda3f8c876be
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-14 18:14:35 +0900

    issue#15 lint app/lib

--- app/lib.tsx
+++ app/lib.tsx
@@ -1,11 +1,11 @@
-import { Pencil, Stamp } from "lucide-react";
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/stories/Button";
-import { NavBarItemProps } from "./types";
+import { Pencil, Stamp } from 'lucide-react';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/stories/Button';
+import { NavBarItemProps } from './types';
 
 export const staticNavBarItems: NavBarItemProps[] = [
   {
-    href: "/",
+    href: '/',
     Inner: (
       <Avatar className="w-7 h-7">
         <AvatarImage src="/kkuk-kkuk.svg" />
@@ -14,15 +14,11 @@ export const staticNavBarItems: NavBarItemProps[] = [
   },
 ];
 
-export const getSignedNavBarItems = ({
-  image = "/kkuk-kkuk.svg",
-}: {
-  image?: string;
-}): NavBarItemProps[] => [
-  { href: "/kits/new", Inner: <Pencil /> },
-  { href: "/rallies", Inner: <Stamp /> },
+export const getSignedNavBarItems = ({ image }: { image?: string }): NavBarItemProps[] => [
+  { href: '/kits/new', Inner: <Pencil /> },
+  { href: '/rallies', Inner: <Stamp /> },
   {
-    href: "/my",
+    href: '/my',
     Inner: (
       <Avatar className="w-7 h-7 border">
         <AvatarImage src={image} />
@@ -33,13 +29,7 @@ export const getSignedNavBarItems = ({
 
 export const notSignedNavBarItems: NavBarItemProps[] = [
   {
-    href: "/signin",
-    Inner: (
-      <Button
-        label="로그인"
-        type="button"
-        className="text-sm px-4 h-fit py-1.5 rounded-lg"
-      />
-    ),
+    href: '/signin',
+    Inner: <Button label="로그인" type="button" className="text-sm px-4 h-fit py-1.5 rounded-lg" />,
   },
 ];
