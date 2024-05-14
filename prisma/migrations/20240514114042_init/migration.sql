6491c99df65e1a418227d5e77ad30be1a2d81128
Madang Garden <chooi9522@gmail.com>
2024-05-14 20:57:49 +0900

    issue/#12 Prisma 초기 설정 및 스키마 작성, RDS 마이그레이션

---
+++ prisma/migrations/20240514114042_init/migration.sql
@@ -0,0 +1,73 @@
+-- CreateEnum
+CREATE TYPE "RallyStatus" AS ENUM ('active', 'inactive');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "profileImage" TEXT,
+    "nickname" TEXT,
+    "authToken" TEXT,
+    "authProvider" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Kit" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "tag" TEXT,
+    "thumbnailImage" TEXT,
+    "rewardImage" TEXT,
+    "uploaderId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Kit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Stamp" (
+    "id" TEXT NOT NULL,
+    "kitId" INTEGER NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "Stamp_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Rally" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" "RallyStatus" NOT NULL,
+    "kitId" INTEGER NOT NULL,
+    "starterId" TEXT NOT NULL,
+    "stampCount" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Rally_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Kit" ADD CONSTRAINT "Kit_uploaderId_fkey" FOREIGN KEY ("uploaderId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Stamp" ADD CONSTRAINT "Stamp_kitId_fkey" FOREIGN KEY ("kitId") REFERENCES "Kit"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Rally" ADD CONSTRAINT "Rally_kitId_fkey" FOREIGN KEY ("kitId") REFERENCES "Kit"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Rally" ADD CONSTRAINT "Rally_starterId_fkey" FOREIGN KEY ("starterId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
