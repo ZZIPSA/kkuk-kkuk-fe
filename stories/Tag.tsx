f64ba24f53a12889ec3de5d75bb4e2804b331c80
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-14 16:41:25 +0900

    issue#8 style tag

--- stories/Tag.tsx
+++ stories/Tag.tsx
@@ -13,8 +13,8 @@ const variants: Record<Uses, BadgeVariant> = {
 } as const;
 
 const classNames = {
-  [Uses.default]: 'bg-primary/40 text-primary font-extralight hover:text-white focus:ring-primary',
-  [Uses.selected]: 'bg-lime-500/50 text-lime-800 font-extralight hover:text-white hover:bg-lime-500/80 focus:ring-lime-500',
+  [Uses.default]: 'bg-primary/40 text-primary hover:text-white',
+  [Uses.selected]: 'bg-lime-500/50 text-lime-800 hover:text-white hover:bg-lime-500/80',
 } as const;
 
 interface TagProps extends ButtonBadgeProps {
@@ -37,7 +37,7 @@ interface TagProps extends ButtonBadgeProps {
  */
 export const Tag = ({ use = Uses.default, label, className, ...props }: TagProps) => {
   return (
-    <ButtonBadge className={cn(classNames[use], className)} variant={variants[use]} {...props}>
+    <ButtonBadge className={cn('font-extralight', classNames[use], className)} variant={variants[use]} {...props}>
       {'#'}
       {label}
     </ButtonBadge>
