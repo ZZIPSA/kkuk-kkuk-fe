4e9e5fd6043d10abaf3d731044c39823f4e4404b
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-15 03:07:49 +0900

    issue#8 remove description

--- app/kits/page.tsx
+++ app/kits/page.tsx
@@ -7,8 +7,8 @@ export default async function KitsPage() {
   return (
     <main className="w-full p-4 pt-6 grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-3 lg:grid-cols-4 lg:gap-4">
       <h1 className="text-xl font-bold text-foreground col-span-full">등록된 키트</h1>
-      {kits?.map(({ id, title, description, thumbnailImage, tags, uploader }) => (
-        <KitCard id={id} key={id} title={title} description={description} thumbnailImage={thumbnailImage} tags={tags} uploader={uploader} />
+      {kits?.map(({ id, title, thumbnailImage, tags, uploader }) => (
+        <KitCard id={id} key={id} title={title} thumbnailImage={thumbnailImage} tags={tags} uploader={uploader} />
       ))}
     </main>
   );
