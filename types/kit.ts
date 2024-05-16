7298235143f8e850fba0b5f101f49b638c47eb27
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-16 18:32:08 +0900

    issue#26 define KitCardsInfo type

--- types/kit.ts
+++ types/kit.ts
@@ -3,3 +3,4 @@ import type { prisma } from '@/lib/prisma';
 import type { kitCardSelect } from '@/lib/prisma';
 
 export type KitCardInfo = NonNullable<Prisma.Result<typeof prisma.kit, { select: typeof kitCardSelect }, 'findFirst'>>;
+export type KitCardsInfo = Prisma.Result<typeof prisma.kit, { select: typeof kitCardSelect }, 'findMany'>;
