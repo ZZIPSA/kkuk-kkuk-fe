341152a6c0fe44cefd97cd0c4dc4233cac9e6e74
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-19 19:46:04 +0900

    issue/#7 move icons to assets/

--- lib/icons.tsx
+++ lib/icons.tsx
@@ -1,3 +1,3 @@
-export { default as Pencil } from '@/public/pencil.svg';
-export { default as Stamp } from '@/public/stamp.svg';
+export { default as Pencil } from '@/assets/pencil.svg';
+export { default as Stamp } from '@/assets/stamp.svg';
 export { default as Trash } from '@/assets/trash.svg';
