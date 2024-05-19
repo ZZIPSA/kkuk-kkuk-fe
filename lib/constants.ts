1c490facb6332830c06d871ac7dd27c9034e363b
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-19 18:57:04 +0900

    issue/#38 merge lib/defaults.ts to lib/constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,2 @@
 export const DEFAULT_PROFILE = '/default-profile.svg';
+export const DEFAULT_USER_NICKNAME = '찾을 수 없는 사용자';
--- lib/defaults.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_USER_NICKNAME = '찾을 수 없는 사용자';
-export const DEFAULT_PROFILE = '/icon';
