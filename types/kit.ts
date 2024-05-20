f05a84481dcafaa13a018b878ae053c313472bfa
Madang Garden <chooi9522@gmail.com>
2024-05-21 00:42:05 +0900

    issue/#20 refactor:  불필요한 타입 삭제

--- types/kit.ts
+++ types/kit.ts
@@ -3,4 +3,3 @@ import { KitModel, UserModel } from './models';
 type KitCardUploader = { uploader: Pick<UserModel, 'profileImage' | 'nickname'> };
 
 export type KitCardInfo = Pick<KitModel, 'id' | 'title' | 'thumbnailImage' | 'tags'> & KitCardUploader;
-export type KitCardsInfo = KitCardInfo[];
