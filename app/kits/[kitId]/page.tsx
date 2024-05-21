21ab3a1283557901034552e81848f17b3ffefd2f
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-21 19:45:56 +0900

    issue/#41 add KitCard in kit page

--- app/kits/[kitId]/page.tsx
+++ app/kits/[kitId]/page.tsx
@@ -1,7 +1,24 @@
-export default async function KitPage({
-  params: { kitId },
-}: {
-  params: { kitId: string };
-}) {
-  return <main></main>;
+import KitCard, { KitCardVariants } from '@/components/KitCard';
+import { DEFAULT_KIT_THUMBNAIL } from '@/lib/constants';
+import { notFound } from 'next/navigation';
+
+export default async function KitPage({ params: { kitId } }: { params: { kitId: string } }) {
+  console.log(`${process.env.API_URL}/api/kits/${kitId}`);
+  const { data: kit } = await fetch(`${process.env.API_URL}/api/kits/${kitId}`).then((res) => res.json());
+  if (!kit) return notFound();
+  const { title, description, tags, thumbnailImage, uploader } = kit;
+
+  return (
+    <main className="grid grid-cols-1 px-4 py-6">
+      <KitCard
+        id={kitId}
+        title={title}
+        description={description ?? ''}
+        tags={tags}
+        thumbnailImage={thumbnailImage ?? DEFAULT_KIT_THUMBNAIL}
+        uploader={uploader}
+        variant={KitCardVariants.description}
+      />
+    </main>
+  );
 }
