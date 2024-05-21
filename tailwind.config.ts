f29992486602e34419233b7370df253485fa439c
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-22 03:51:56 +0900

    issue/#49 load rally route background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,6 +84,9 @@ const config = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      backgroundImage: {
+        'rally-route': "url('/rally-route.svg')",
+      },
     },
   },
   plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar-hide')],
