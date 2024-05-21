abd09f58a891868739704b6f3c734136bc289184
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-21 19:55:53 +0900

    issue/#40 replace kitId with id

--- app/kits/[id]/page.tsx
+++ app/kits/[id]/page.tsx
@@ -1,16 +1,16 @@
 import KitCard, { KitCardVariants } from '@/components/KitCard';
 import { notFound } from 'next/navigation';
 
-export default async function KitPage({ params: { kitId } }: { params: { kitId: string } }) {
-  console.log(`${process.env.API_URL}/api/kits/${kitId}`);
-  const { data: kit } = await fetch(`${process.env.API_URL}/api/kits/${kitId}`).then((res) => res.json());
+export default async function KitPage({ params: { id } }: { params: { id: string } }) {
+  console.log(`${process.env.API_URL}/api/kits/${id}`);
+  const { data: kit } = await fetch(`${process.env.API_URL}/api/kits/${id}`).then((res) => res.json());
   if (!kit) return notFound();
   const { title, description, tags, thumbnailImage, uploader } = kit;
 
   return (
     <main className="grid grid-cols-1 px-4 py-6">
       <KitCard
-        id={kitId}
+        id={id}
         title={title}
         description={description ?? ''}
         tags={tags}
