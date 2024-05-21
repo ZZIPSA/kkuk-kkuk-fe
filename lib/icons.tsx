c9da16f2dcf52ab3f212f2382f07a9c31137ee7d
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-21 19:09:42 +0900

    issue/#41 load icons

--- lib/icons.tsx
+++ lib/icons.tsx
@@ -1,3 +1,5 @@
 export { default as Pencil } from '@/assets/pencil.svg';
 export { default as Stamp } from '@/assets/stamp.svg';
 export { default as Trash } from '@/assets/trash.svg';
+export { default as Heart } from '@/assets/heart.svg';
+export { default as Bookmark } from '@/assets/bookmark.svg';
