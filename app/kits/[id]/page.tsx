3d5c21c67df30b5960464e354a6fdf62d3f4e341
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-22 04:11:59 +0900

    issue/#51 add StampsPreview component

--- app/kits/[id]/page.tsx
+++ app/kits/[id]/page.tsx
@@ -1,6 +1,7 @@
 import KitCard, { KitCardVariants } from '@/components/KitCard';
 import { notFound } from 'next/navigation';
 import RallyPreview from './components/RallyPreview';
+import StampsPreview from './components/StampsPreview';
 
 export default async function KitPage({ params: { id } }: { params: { id: string } }) {
   console.log(`${process.env.API_URL}/api/kits/${id}`);
@@ -20,6 +21,7 @@ export default async function KitPage({ params: { id } }: { params: { id: string
         variant={KitCardVariants.description}
       />
       <RallyPreview stamps={stamps} />
+      <StampsPreview stamps={stamps} />
     </main>
   );
 }
