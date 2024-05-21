415b014d9e369cb6b1c5d53680480a17ba2fdd08
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-22 03:56:40 +0900

    issue/#49 get stamps from api

--- app/kits/[id]/page.tsx
+++ app/kits/[id]/page.tsx
@@ -5,7 +5,7 @@ export default async function KitPage({ params: { id } }: { params: { id: string
   console.log(`${process.env.API_URL}/api/kits/${id}`);
   const { data: kit } = await fetch(`${process.env.API_URL}/api/kits/${id}`).then((res) => res.json());
   if (!kit) return notFound();
-  const { title, description, tags, thumbnailImage, uploader } = kit;
+  const { title, description, tags, thumbnailImage, uploader, stamps } = kit;
 
   return (
     <main className="grid grid-cols-1">
