60410c18590fa67000ee50529ec0ff0fc24a7d9c
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-22 15:22:38 +0900

    issue/#56 define MenuTabs component

---
+++ app/my/(info)/components/MenuTabs/index.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { items } from './lib';
+import MenuTab from './MenuTab';
+
+export default function MenuTabs() {
+  const path = usePathname();
+  console.log(path);
+  return (
+    <section className="flex justify-center border-b border-gray-200 pt-2 sticky top-0">
+      {items.map(({ label, href }) => (
+        <MenuTab key={href} label={label} href={href} path={path} />
+      ))}
+    </section>
+  );
+}
