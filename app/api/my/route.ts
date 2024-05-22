8c77f99dd54b7d89590d0d425d9945bdc86f83b7
Madang Garden <chooi9522@gmail.com>
2024-05-22 15:59:51 +0900

    issue/#33 refactor: 스키마 수정 및 파일 내 import문 순서 수정

--- app/api/my/route.ts
+++ app/api/my/route.ts
@@ -1,5 +1,5 @@
-import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
 
 //TODO: 로그인 구현 후 id는 세션에서 취득
 export async function GET() {
