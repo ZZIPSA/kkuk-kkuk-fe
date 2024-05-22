80a19f8dc1288be81454badc2e23c9bd8f62c44c
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-22 11:43:21 +0900

    issue/#53 select user columns

--- app/api/my/route.ts
+++ app/api/my/route.ts
@@ -10,6 +10,7 @@ export async function GET() {
 
   const data = await prisma.user.findUnique({
     where: { id },
+    select: { profileImage: true, nickname: true, accounts: { select: { provider: true, userId: true } }, rallies: true },
   });
 
   return NextResponse.json({ data });
