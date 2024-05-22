82c804453aef4de56917e00fdf33ada8b736aa84
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-22 11:42:54 +0900

    issue/#53 get user ID from auth

--- app/api/my/route.ts
+++ app/api/my/route.ts
@@ -1,13 +1,16 @@
+import { auth } from '@/auth';
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
 //TODO: 로그인 구현 후 id는 세션에서 취득
 export async function GET() {
-  const id = 'clw812wi3000oftre6au63u1f';
+  const { user } = await auth();
+  if (!user) return NextResponse.redirect('/signin');
+  const { id } = user;
 
-  const user = await prisma.user.findUnique({
+  const data = await prisma.user.findUnique({
     where: { id },
   });
 
-  return NextResponse.json({ data: user });
+  return NextResponse.json({ data });
 }
