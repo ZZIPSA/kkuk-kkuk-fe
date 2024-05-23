9a67d60d3ffaaef56ee784a38d9b5cd51d1dfab9
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-23 20:22:50 +0900

    issue/#63 show nickname when default

--- components/UserInfo/index.tsx
+++ components/UserInfo/index.tsx
@@ -22,7 +22,7 @@ export default async function UserInfo({ variant = UserInfoVariant.default }: Us
     <section className="flex flex-col py-6 px-4 gap-4">
       <div className="grid grid-cols-[64px_auto] w-full gap-x-2">
         <ProfileImage profileImage={profileImage} nickname={nickname} />
-        <h1 className="font-bold w-full">{nickname}</h1>
+        {variant === UserInfoVariant.default && <h1 className="font-bold w-full">{nickname}</h1>}
         {twitterAccount && (
           <Badge variant="secondary" className="w-fit text-xs font-normal gap-2">
             <span className="text-2xl">𝕏</span>@{twitterAccount.userId}
