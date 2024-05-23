7bc031ddfcba298916341382cff9f4ff196a2684
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-23 16:20:39 +0900

    issue/#53 get joined rallies

---
+++ app/api/my/joins/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import { RallyStatus } from '@prisma/client';
+import { auth, signIn } from '@/auth';
+import { prisma } from '@/lib/prisma';
+import { MyRally } from '@/types/Rally';
+
+export async function GET() {
+  const { user } = await auth();
+  if (!user) return signIn();
+  const { id } = user;
+
+  const data: MyRally[] = await prisma.rally.findMany({
+    where: { starterId: id, status: RallyStatus.active },
+    select: {
+      id: true,
+      stampCount: true,
+      kit: {
+        select: {
+          thumbnailImage: true,
+          title: true,
+          _count: {
+            select: {
+              stamps: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return NextResponse.json({ data });
+}
