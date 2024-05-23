69081caa631e673b8f0e2ff8eb22aa0d0cdedfa1
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-23 22:39:34 +0900

    issue/#63 add UserSettings component

--- app/my/settings/page.tsx
+++ app/my/settings/page.tsx
@@ -1,10 +1,12 @@
 import UserInfo from '@/components/UserInfo';
 import { UserInfoVariant } from '@/components/UserInfo/variants';
+import { UserSettings } from './components/UserSettings';
 
 export default async function SettingsPage() {
   return (
     <main>
       <UserInfo variant={UserInfoVariant.settings} />
+      <UserSettings />
     </main>
   );
 }
