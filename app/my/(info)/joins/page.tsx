bd6e7f72b67920a60ec3d43ae18cd9792f8f1989
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-23 16:22:24 +0900

    issue/#53 add RallyCard to joined rallies page

--- app/my/(info)/joins/page.tsx
+++ app/my/(info)/joins/page.tsx
@@ -1,3 +1,26 @@
-export default async function JoinedPage() {
-  return <main></main>;
+import { MyRally } from '@/types/Rally';
+import RallyCard from '@/components/RallyCard';
+import Link from 'next/link';
+
+export default async function JoinsPage() {
+  const { data: rallies }: { data: MyRally[] } = await fetch(process.env.API_URL + '/api/my/joins').then((res) => res.json());
+  return (
+    <article className="px-4 py-6 grid grid-cols-2 gap-x-2 gap-y-4">
+      {rallies.map(
+        ({
+          id,
+          stampCount,
+          kit: {
+            thumbnailImage,
+            title,
+            _count: { stamps },
+          },
+        }) => (
+          <Link key={id} href={`/rally/${id}`}>
+            <RallyCard stampCount={stampCount} thumbnailImage={thumbnailImage} title={title} stamps={stamps} />
+          </Link>
+        ),
+      )}
+    </article>
+  );
 }
