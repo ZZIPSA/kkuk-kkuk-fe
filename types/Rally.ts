c93ca37eebc00692c34f40a5f50cd0554d222bb4
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-23 16:18:55 +0900

    issue/#53 define MyRally type

--- types/Rally.ts
+++ types/Rally.ts
@@ -2,3 +2,13 @@ import { RallyModel, UserModel } from './models';
 
 type RallyStarter = { starter: Pick<UserModel, 'profileImage' | 'nickname'> };
 export type RallyInfo = Pick<RallyModel, 'id' | 'title'> & RallyStarter;
+
+export interface MyRally {
+  id: string;
+  stampCount: number;
+  kit: {
+    title: string;
+    _count: { stamps: number };
+    thumbnailImage: string;
+  };
+}
