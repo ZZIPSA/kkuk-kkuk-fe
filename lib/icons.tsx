b4824a17cd9047d0053be1b408523326615704f1
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-23 19:56:32 +0900

    issue/#63 load settings icon

--- lib/icons.tsx
+++ lib/icons.tsx
@@ -4,3 +4,4 @@ export { default as Trash } from '@/assets/trash.svg';
 export { default as Heart } from '@/assets/heart.svg';
 export { default as Bookmark } from '@/assets/bookmark.svg';
 export { default as Check } from '@/assets/check.svg';
+export { default as Settings } from '@/assets/settings.svg';
