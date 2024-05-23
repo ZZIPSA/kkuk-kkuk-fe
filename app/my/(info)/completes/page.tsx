de577e29116d75de7ce9bd231fcbb6a11cb88c0e
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-23 17:31:37 +0900

    issue/#61 define completes page

--- app/my/(info)/completes/page.tsx
+++ app/my/(info)/completes/page.tsx
@@ -1,3 +1,17 @@
-export default async function CompletedPage() {
-  return <main></main>;
+import { CompletedRally } from '@/types/Rally';
+import RallyCard from '@/components/RallyCard';
+import Link from 'next/link';
+
+export default async function CompletesPage() {
+  const { data: rallies }: { data: CompletedRally[] } = await fetch(process.env.API_URL + '/api/my/completes').then((res) => res.json());
+  console.log({ rallies });
+  return (
+    <article className="px-4 py-6 grid grid-cols-2 gap-x-2 gap-y-4">
+      {rallies.map(({ id, updatedAt, kit: { thumbnailImage, title } }) => (
+        <Link key={id} href={`/rally/${id}`}>
+          <RallyCard thumbnailImage={thumbnailImage} title={title} updatedAt={updatedAt} />
+        </Link>
+      ))}
+    </article>
+  );
 }
