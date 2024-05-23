5f0e8e340a8d2cf8f194ee7f36cbc34c7e2803d3
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-23 18:19:39 +0900

    issue/#62 get upload kits

---
+++ app/api/my/uploads/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server';
+import { auth, signIn } from '@/auth';
+import { prisma } from '@/lib/prisma';
+import { KitCardInfo } from '@/types/Kit';
+
+export async function GET() {
+  const { user } = await auth();
+  if (!user) return signIn();
+  const { id } = user;
+
+  const data: KitCardInfo[] = await prisma.kit.findMany({
+    where: { uploaderId: id },
+    select: {
+      id: true,
+      title: true,
+      thumbnailImage: true,
+      tags: true,
+      uploader: {
+        select: { profileImage: true, nickname: true },
+      },
+    },
+  });
+
+  return NextResponse.json({ data });
+}
