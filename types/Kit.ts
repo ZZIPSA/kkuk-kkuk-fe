2c152ccc87ff71403aedaa6f77963613199de4f1
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-24 19:08:48 +0900

    issue/#68 define KitCardInfo type using KitResult

--- types/Kit.ts
+++ types/Kit.ts
@@ -4,4 +4,5 @@ type KitUploader = { uploader: Pick<UserModel, 'id' | 'nickname' | 'profileImage
 type KitStamps = { stamps: Pick<StampModel, 'id' | 'image'>[] };
 export type KitResult = Pick<KitModel, 'id' | 'title' | 'description' | 'tags' | 'thumbnailImage' | 'rewardImage'> & KitUploader & KitStamps;
 
-export type KitCardInfo = Pick<KitModel, 'id' | 'title' | 'thumbnailImage' | 'tags'> & KitCardUploader;
+type KitCardUploader = { uploader: Pick<NonNullable<UserModel>, 'profileImage' | 'nickname'> };
+export type KitCardInfo = Pick<KitResult, 'id' | 'title' | 'thumbnailImage' | 'tags'> & KitCardUploader;
