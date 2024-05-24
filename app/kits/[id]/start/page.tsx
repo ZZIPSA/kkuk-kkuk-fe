c48ff8d8ba0ec7a16fd46d9485e35e3de2222790
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-24 19:34:35 +0900

    issue/#68 fetch kit data

--- app/kits/[id]/start/page.tsx
+++ app/kits/[id]/start/page.tsx
@@ -1,7 +1,12 @@
-export default async function KitStartPage({
-  params: { kitId },
-}: {
-  params: { kitId: string };
-}) {
-  return <main></main>;
+import { notFound } from 'next/navigation';
+
+export default async function KitStartPage({ params: { id } }: { params: { id: string } }) {
+  const { data: kit } = await fetch(`${process.env.API_URL}/api/kits/${id}`).then((res) => res.json());
+  if (!kit) return notFound();
+  const { title, description, tags, thumbnailImage, uploader } = kit;
+
+  return (
+    <main className="py-6 px-4 flex flex-col gap-6">
+    </main>
+  );
 }
