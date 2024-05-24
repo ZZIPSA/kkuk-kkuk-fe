7da4b6fdce475816921c8002b05a50a03e138446
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-24 13:06:36 +0900

    issue/#67 define rallies page

---
+++ app/rallies/page.tsx
@@ -0,0 +1,26 @@
+import RallyCard from '@/components/RallyCard';
+import { JoinedRally } from '@/types/Rally';
+import Link from 'next/link';
+
+export default async function RalliesPage() {
+  const { data: rallies }: { data: JoinedRally[] } = await fetch(process.env.API_URL + '/api/my/joins').then((res) => res.json());
+  return (
+    <main className="px-4 py-6 grid grid-cols-2 gap-x-2 gap-y-4">
+      {rallies.map(
+        ({
+          id,
+          stampCount,
+          kit: {
+            thumbnailImage,
+            title,
+            _count: { stamps },
+          },
+        }) => (
+          <Link key={id} href={`/rally/${id}`}>
+            <RallyCard stampCount={stampCount} thumbnailImage={thumbnailImage} title={title} stamps={stamps} />
+          </Link>
+        ),
+      )}
+    </main>
+  );
+}
