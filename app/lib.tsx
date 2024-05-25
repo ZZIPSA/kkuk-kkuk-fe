f488d807f50124d07051389b34f2bf5abfafe203
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-25 17:13:17 +0900

    issue/#73 replace useSession with useUser

--- app/lib.tsx
+++ app/lib.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/stories/Button';
 import { NavBarItemProps } from './types';
 import { Pencil, Settings, Stamp } from '@/lib/icons';
 import { DEFAULT_PROFILE } from '@/lib/constants';
-import { UserModel } from '@/types/models';
+import { User } from 'next-auth';
 
 export const headerLogoItems: NavBarItemProps[] = [
   {
@@ -16,14 +16,14 @@ export const headerLogoItems: NavBarItemProps[] = [
   },
 ];
 
-export const getHeaderUserMenuItems = (user?: Pick<UserModel, 'profileImage'>): NavBarItemProps[] => [
+export const getHeaderUserMenuItems = (user?: Pick<User, 'image'>): NavBarItemProps[] => [
   { href: '/kits/new', Inner: <Pencil className="h-8 w-8 fill-foreground" />, isGuest: false },
   { href: '/rallies', Inner: <Stamp className="h-8 w-8 stroke-foreground" />, isGuest: false },
   {
     href: '/my',
     Inner: (
       <Avatar className="w-8 h-8 border">
-        <AvatarImage src={user?.profileImage ?? DEFAULT_PROFILE} />
+        <AvatarImage src={user?.image ?? DEFAULT_PROFILE} />
       </Avatar>
     ),
     isGuest: false,
@@ -31,7 +31,7 @@ export const getHeaderUserMenuItems = (user?: Pick<UserModel, 'profileImage'>): 
   },
   { href: '/my/settings', Inner: <Settings className="h-8 w-8 fill-foreground" />, showAtMyPage: true },
   {
-    href: '/signin',
+    href: '/api/auth/signin',
     Inner: <Button label="로그인" type="button" className="text-base px-4 py-2 h-fit rounded-lg" />,
     isGuest: true,
   },
