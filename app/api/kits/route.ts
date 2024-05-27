64d8637fd9fdbc5fa0230518ba5ef1361010ecf5
Madang Garden <chooi9522@gmail.com>
2024-05-28 01:37:07 +0900

    issue/#50 feat: 블러 기능 추가 및 업로드 관련 로직 재정리

--- app/api/kits/route.ts
+++ app/api/kits/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 import { auth } from '@/auth';
+import { S3Manager } from '@/lib/services/s3';
 
 export async function GET() {
   const kits = await prisma.kit.findMany({});
@@ -9,16 +10,18 @@ export async function GET() {
 }
 
 export async function POST(request: Request) {
-  // TODO: auth, 필수 항목 미들웨어 구현
-  const session = await auth();
-  const currentUser = session?.user;
+  // TODO: auth, 필수 항목 검증 미들웨어 구현
+  const s3 = new S3Manager();
+  // const session = await auth();
+  // const currentUser = session?.user;
 
-  if (!currentUser) return NextResponse.json({ error: '로그인 해주세요.' }, { status: 400 });
+  // if (!currentUser) return NextResponse.json({ error: '로그인 해주세요.' }, { status: 400 });
 
-  const uploaderId = currentUser.id;
-  const { title, description, imageUrls, thumbnailImage, rewardImage, tags } = await request.json();
+  // const uploaderId = currentUser.id;
+  const uploaderId = 'clwnjgnp10004dnxeusl9exo3';
 
-  if (!title || !Array.isArray(imageUrls) || !thumbnailImage || !rewardImage || !uploaderId) {
+  const { title, description, imageUrls, thumbnailImage, rewardImage, blurredImage, tags } = await request.json();
+  if (!title || !Array.isArray(imageUrls) || !thumbnailImage || !rewardImage || !blurredImage || !uploaderId) {
     return NextResponse.json({ error: '필수 항목을 입력해주세요.' }, { status: 400 });
   }
 
@@ -28,24 +31,35 @@ export async function POST(request: Request) {
     },
     take: 1,
   });
-  const lastId = parseInt(lastKit[0].id, 10);
-  const newId = (lastId + 1).toString().padStart(7, '0');
+  const lastKitId = parseInt(lastKit[0].id, 10);
+  const newKitId = (lastKitId + 1).toString().padStart(7, '0');
 
   try {
+    // blur 이미지 등 메타데이터는 별도 이동 필요
+    const newKitUrls = await Promise.all(
+      imageUrls.map(async (url: string, index: number) => {
+        const targetKey = s3.extractS3Key(url);
+        const newUrl = `${uploaderId}/${newKitId}/${index}`;
+
+        await s3.moveObject(targetKey, newUrl);
+        // http 붙여야함
+        return newUrl;
+      }),
+    );
+
+    const newBlurUrl = await s3.copyObject(s3.extractS3Key(blurredImage), `${uploaderId}/${newKitId}/6`);
+
     const kit = await prisma.kit.create({
       data: {
-        id: newId,
+        id: newKitId,
         title,
         description,
         stamps: {
-          create: imageUrls.map((url: string) => {
-            return {
-              image: url,
-            };
-          }),
+          create: newKitUrls.map((url) => ({ image: url })),
         },
         rewardImage,
         thumbnailImage,
+        blurredImage,
         tags,
         uploaderId,
       },
--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,22 +1,25 @@
 'use client';
 
 import { useState } from 'react';
+import { preuploadStamp, uploadBlurImage } from '../api/kits/assets/actions';
 
-/**
- * 업로드를 위한 예시 페이지입니다. 배포 전에 삭제되어야 합니다.
- * 타이틀이나 설명, 파일 크기 및 확장자에 대한 검증은 하고 있지않습니다.
- * 선업로드 방식이 적용되어있지 않습니다.
- * API를 보낼 때, 태그가 추가되어야 합니다.
- *
- */
+const arrayBufferToBase64 = (buffer: ArrayBuffer) => {
+  let binary = '';
+  const bytes = new Uint8Array(buffer);
+  const len = bytes.byteLength;
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return window.btoa(binary);
+};
 
-// TODO: 프론트 업로드 구현 완료 시 이 파일 전체 삭제
 const UploadPage = () => {
   const [files, setFiles] = useState<(File | null)[]>(Array(6).fill(null));
   const [kitTitle, setKitTitle] = useState('');
   const [kitDescription, setKitDescription] = useState('');
   const [uploading, setUploading] = useState(false);
   const [uploadedUrls, setUploadedUrls] = useState<string[]>([]);
+  const [blurredUrl, setBlurredUrl] = useState<string | null>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
     const newFiles = [...files];
@@ -25,58 +28,30 @@ const UploadPage = () => {
   };
 
   const handleUpload = async () => {
-    if (files.filter((file) => file).length === 0 || !kitTitle) {
-      alert('Please select files and provide a kit title');
+    if (files.includes(null) || !kitTitle) {
+      alert('6개의 파일을 선택하고 키트 제목을 입력해주세요.');
       return;
     }
 
     setUploading(true);
     try {
       const imageUrls = await Promise.all(
-        files
-          .filter((file) => file)
-          .map(async (file) => {
-            if (!file) return '';
-            // 1. presigned URL 요청
-            const response = await fetch('/api/s3/presign', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                fileName: file.name,
-                fileType: file.type,
-              }),
-            });
-
-            if (!response.ok) {
-              throw new Error('Failed to get presigned URL');
-            }
-
-            const data = await response.json();
-
-            // 2. 파일 업로드
-            const uploadResponse = await fetch(data.presignedUrl, {
-              method: 'PUT',
-              headers: {
-                'Content-Type': file.type,
-              },
-              body: file,
-            });
-
-            if (!uploadResponse.ok) {
-              throw new Error('Failed to upload file');
-            }
-
-            // 업로드된 파일의 URL 반환
-            return data.presignedUrl.split('?')[0];
-          }),
+        files.map(async (file, index) => {
+          if (!file) throw new Error('올바른 파일을 업로드 해주세요.');
+          const bufferFile = await file.arrayBuffer();
+          const base64File = arrayBufferToBase64(bufferFile);
+          if (index === 5) {
+            const blurUrl = await uploadBlurImage(base64File, index);
+            console.log('blurredUrl', blurUrl);
+            setBlurredUrl(blurUrl);
+          }
+          return preuploadStamp(base64File, index);
+        }),
       );
 
       setUploadedUrls(imageUrls);
 
-      // 3. Kit 생성 요청
-      const createKitResponse = await fetch('/api/kits', {
+      const createKitResponse = await fetch(`/api/kits`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -86,6 +61,7 @@ const UploadPage = () => {
           description: kitDescription,
           thumbnailImage: imageUrls[0],
           rewardImage: imageUrls[5],
+          blurredImage: blurredUrl,
           imageUrls,
         }),
       });
@@ -94,8 +70,6 @@ const UploadPage = () => {
         throw new Error('Failed to create kit');
       }
 
-      const kitData = await createKitResponse.json();
-
       alert('키트를 생성했습니다!'); // TODO: 프론트 구현후 삭제
 
       // 초기화
@@ -103,6 +77,7 @@ const UploadPage = () => {
       setKitTitle('');
       setKitDescription('');
       setUploadedUrls([]);
+      setBlurredUrl(null);
     } catch (error) {
       console.error('Error uploading files', error);
       alert('파일을 업로드 하지 못했습니다.');
@@ -124,20 +99,6 @@ const UploadPage = () => {
       <button onClick={handleUpload} disabled={uploading}>
         {uploading ? 'Uploading...' : 'Upload'}
       </button>
-      {uploadedUrls.length > 0 && (
-        <div>
-          <h2>Uploaded URLs:</h2>
-          <ul>
-            {uploadedUrls.map((url, index) => (
-              <li key={index}>
-                <a href={url} target="_blank" rel="noopener noreferrer">
-                  {url}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
     </div>
   );
 };
