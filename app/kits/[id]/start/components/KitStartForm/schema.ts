f4ae011b23d49e141eb39cbe7898534db48372d7
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-28 14:09:40 +0900

    issue/#68 require title

--- app/kits/[id]/start/components/KitStartForm/schema.ts
+++ app/kits/[id]/start/components/KitStartForm/schema.ts
@@ -1,17 +1,25 @@
 import { z } from 'zod';
 
-const formSchema = z.object({
-  title: z
-    .string()
-    .min(2, {
-      message: '랠리 목표는 2글자 이상이어야 합니다.',
-    })
-    .max(20, {
-      message: '랠리 목표는 20글자 이하이어야 합니다.',
+const formSchema = z
+  .object({
+    title: z
+      .string({
+        required_error: '랠리 목표를 입력해주세요.',
+      })
+      .min(2, {
+        message: '랠리 목표는 2글자 이상이어야 합니다.',
+      })
+      .max(20, {
+        message: '랠리 목표는 20글자 이하이어야 합니다.',
+      }),
+    description: z.string().max(200, {
+      message: '랠리 설명은 200글자 이하이어야 합니다.',
     }),
-  description: z.string().max(200, {
-    message: '랠리 설명은 200글자 이하이어야 합니다.',
-  }),
-});
-
+  })
+  .required({
+    title: true,
+  });
+// .required({
+//   title:
+// });
 export default formSchema;
