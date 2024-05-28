7ca21a8b12bbfe5dee2b40e4d040d03553cd2df0
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-29 01:10:22 +0900

    issue/#82 define styles for StampPreview

--- app/kits/new/components/NewKitForm/Stamps/styles.ts
+++ app/kits/new/components/NewKitForm/Stamps/styles.ts
@@ -5,3 +5,7 @@ export const stampEmptyStampGiftIconStyles = 'fill-white size-full aspect-square
 export const stampEmptyStampPictureIconStyles = 'fill-grey-100 size-full aspect-square';
 export const stampEmptyStampCirclePlusIconStyles = 'absolute size-5 -bottom-1 -right-1 bg-grey-400 p-0.5 rounded-full fill-white';
 
+// StampPreview
+export const stampPreviewStyles = 'relative w-full h-full';
+export const stampPreviewImageStyles = 'rounded-2xl aspect-square w-full object-cover bg-background';
+export const stampTrashIconStyles = 'absolute w-8 h-8 right-0.5 bottom-0.5 cursor-pointer';
