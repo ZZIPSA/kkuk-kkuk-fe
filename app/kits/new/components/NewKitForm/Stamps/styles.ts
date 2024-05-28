39a5f120e8c45f2ff5a616ca05cf82069f2defe6
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-29 01:09:25 +0900

    issue/#82 define styles for EmptyStamp

---
+++ app/kits/new/components/NewKitForm/Stamps/styles.ts
@@ -0,0 +1,7 @@
+// EmptyStamp
+export const stampEmptyStampStyles = 'text-grey-400 text-sm font-bold size-full flex flex-col gap-4 justify-center items-center';
+export const stampEmptyStampIconStyles = 'pt-1 size-5/12 relative';
+export const stampEmptyStampGiftIconStyles = 'fill-white size-full aspect-square';
+export const stampEmptyStampPictureIconStyles = 'fill-grey-100 size-full aspect-square';
+export const stampEmptyStampCirclePlusIconStyles = 'absolute size-5 -bottom-1 -right-1 bg-grey-400 p-0.5 rounded-full fill-white';
+
