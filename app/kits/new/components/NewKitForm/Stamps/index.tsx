05bb6b84870cbae283c14aee8ebc0c4642f7d505
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-29 01:19:32 +0900

    issue/#82 modify label text

--- app/kits/new/components/NewKitForm/Stamps/index.tsx
+++ app/kits/new/components/NewKitForm/Stamps/index.tsx
@@ -12,7 +12,7 @@ const Stamps: StampsField = ({ control, stampsRef }) => (
     name="stamps"
     render={({ field: { onChange, ...field }, fieldState }) => (
       <FormItem>
-        <FormLabel aria-required>스탬프</FormLabel>
+        <FormLabel aria-required>스탬프 이미지 등록</FormLabel>
         <FormDescription className={cn('grid grid-cols-2 gap-4')}>
           <span className="col-span-full">첫번째로 등록된 이미지는 대표이미지 , 마지막 등록된 이미지는 완주용 보상 이미지로 적용됩니다.</span>
           {Array.from({ length: 6 }).map((_, i) => (
