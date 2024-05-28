35c2cad2ea1e231e0bd007c65fb3ad3ce83c9697
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-29 01:14:29 +0900

    issue/#82 redefine Stamps with StampInput

--- app/kits/new/components/NewKitForm/Stamps/index.tsx
+++ app/kits/new/components/NewKitForm/Stamps/index.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image';
 import { FormControl, FormField, FormItem, FormLabel, FormDescription, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
 import { StampsField } from '../types';
-import { Trash } from '@/components/icons';
+import StampInput from './StampInput';
+
+const LAST_INDEX = 5;
 
 const Stamps: StampsField = ({ control, stampsRef }) => (
   <FormField
@@ -14,26 +15,9 @@ const Stamps: StampsField = ({ control, stampsRef }) => (
         <FormLabel aria-required>스탬프</FormLabel>
         <FormDescription className={cn('grid grid-cols-2 gap-4')}>
           <span className="col-span-full">첫번째로 등록된 이미지는 대표이미지 , 마지막 등록된 이미지는 완주용 보상 이미지로 적용됩니다.</span>
-          {field.value &&
-            Array.from(field.value)
-              .filter((_, i) => i < 6)
-              .map((file) => (
-                <div key={file.name} className="relative">
-                  <Image
-                    src={URL.createObjectURL(file)}
-                    className="rounded-2xl aspect-square w-full object-cover"
-                    alt={file.name}
-                    width={100}
-                    height={100}
-                  />
-                  <Trash
-                    className="absolute w-8 h-8 right-1 bottom-1 cursor-pointer"
-                    onClick={() => {
-                      onChange(field.value.filter((f) => f !== file));
-                    }}
-                  />
-                </div>
-              ))}
+          {Array.from({ length: 6 }).map((_, i) => (
+            <StampInput key={i} index={i} total={LAST_INDEX} />
+          ))}
         </FormDescription>
         <FormControl>
           <Input
