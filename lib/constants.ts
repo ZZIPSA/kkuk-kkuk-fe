e98649c9a81b22ecad54c748a1fa0b7541e1ebe3
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-29 14:12:27 +0900

    issue/#77 define TWITTER_ACCOUNT_ID const

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const DEFAULT_PROFILE = '/default-profile.svg';
 export const DEFAULT_KIT_THUMBNAIL = '/kkuk-kkuk.svg';
 export const MY_PAGE_PATH = '/my';
+export const TWITTER_ACCOUNT_ID = '';
