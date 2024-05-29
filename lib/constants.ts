9e22b48f8aab369e0b11baf5014856232dd21bc8
Madang Garden <chooi9522@gmail.com>
2024-05-30 00:30:32 +0900

    issue/#50 refactor: confilct해소

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,8 @@
 export const DEFAULT_PROFILE = '/default-profile.svg';
 export const DEFAULT_KIT_THUMBNAIL = '/kkuk-kkuk.svg';
+export const MY_PAGE_PATH = '/my';
 
+// TODO: BE 상수 분리
 export const REGION = process.env.AWS_REGION!;
 export const ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID!;
 export const SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY!;
