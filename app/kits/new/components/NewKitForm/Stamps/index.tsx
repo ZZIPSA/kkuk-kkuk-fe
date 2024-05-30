15abcc48dfd1a13184d08a8334abc4b57d19e7f1
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-05-30 18:08:03 +0900

    issue/#82 remove useless new line

--- app/kits/new/components/NewKitForm/Stamps/index.tsx
+++ app/kits/new/components/NewKitForm/Stamps/index.tsx
@@ -15,8 +15,7 @@ const Stamps: StampsField = ({ control, stampsRef }) => (
         <FormLabel aria-required>스탬프 이미지 등록</FormLabel>
         <FormDescription className={cn('grid grid-cols-2 gap-4')}>
           <span className="col-span-full">
-            1일차에 등록된 이미지는 대표이미지, 6일차에 등록된 이미지는 완주용 보상 이미지로 적용됩니다. 360 x 360px 이하, 150kb 이하의 이미지를
-            등록할 수 있습니다.
+            1일차에 등록된 이미지는 대표이미지, 6일차에 등록된 이미지는 완주용 보상 이미지로 적용됩니다. 360 x 360px 이하, 150kb 이하의 이미지를 등록할 수 있습니다.
           </span>
           {Array.from({ length: 6 }).map((_, i) => (
             <StampInput key={i} index={i} total={LAST_INDEX} />
