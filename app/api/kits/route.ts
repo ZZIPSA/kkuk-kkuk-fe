4283fe01835cf08305a56ed19fc7b98e87e3138e
Madang Garden <chooi9522@gmail.com>
2024-05-31 20:01:28 +0900

    issue/#76 feat: Kit 카테고리 API 에러처리 및 페이지네이션 추가

--- app/api/kits/route.ts
+++ app/api/kits/route.ts
@@ -1,28 +1,65 @@
 import { NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma';
 import { auth } from '@/auth';
+import { prisma } from '@/lib/prisma';
 import { S3Manager } from '@/lib/services/s3';
-import { BUCKET_NAME, REGION } from '@/lib/constants';
+import { BLURRED_IMAGE_INDEX, REWARD_IMAGE_INDEX, THUMBNAIL_IMAGE_INDEX } from '@/lib/constants';
 
-export async function GET() {
-  const kits = await prisma.kit.findMany({});
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const page = searchParams.get('page');
+    const pageSize = searchParams.get('pageSize');
 
-  return NextResponse.json({ data: kits });
+    if (page && pageSize) {
+      const parsedPage = parseInt(page, 10);
+      const parsedPageSize = parseInt(pageSize, 10);
+      const skip = (parsedPage - 1) * parsedPageSize;
+      const take = parsedPageSize;
+
+      const kits = await prisma.kit.findMany({
+        skip,
+        take,
+      });
+
+      const totalKits = await prisma.kit.count();
+      const totalPages = Math.ceil(totalKits / parsedPageSize);
+
+      return NextResponse.json({
+        data: kits,
+        meta: {
+          page: parsedPage,
+          pageSize: parsedPageSize,
+          totalKits,
+          totalPages,
+        },
+      });
+    } else {
+      const kits = await prisma.kit.findMany({});
+      const totalKits = await prisma.kit.count();
+
+      return NextResponse.json({
+        data: kits,
+        meta: {
+          totalKits,
+        },
+      });
+    }
+  } catch (error) {
+    return NextResponse.json({ error: '서버 에러가 발생했습니다.' }, { status: 500 });
+  }
 }
 
 export async function POST(request: Request) {
   // TODO: auth, 필수 항목 검증 미들웨어 구현
-  const s3 = new S3Manager();
-  // const session = await auth();
-  // const currentUser = session?.user;
+  const session = await auth();
+  const currentUser = session?.user;
 
-  // if (!currentUser) return NextResponse.json({ error: '로그인 해주세요.' }, { status: 400 });
+  if (!currentUser) return NextResponse.json({ error: '로그인 해주세요.' }, { status: 403 });
 
-  // const uploaderId = currentUser.id;
-  // TODO: 테스트용 상수 제거
-  const uploaderId = 'clwnjgnp10004dnxeusl9exo3';
+  const uploaderId = currentUser.id;
 
   const { title, description, imageUrls, thumbnailImage, rewardImage, blurredImage, tags } = await request.json();
+  const s3 = new S3Manager();
 
   if (!title || !Array.isArray(imageUrls) || !thumbnailImage || !rewardImage || !blurredImage || !uploaderId) {
     return NextResponse.json({ error: '필수 항목을 입력해주세요.' }, { status: 400 });
@@ -38,16 +75,7 @@ export async function POST(request: Request) {
   const newKitId = (lastKitId + 1).toString().padStart(7, '0');
 
   try {
-    const newKitUrls = await Promise.all(
-      imageUrls.map(async (url: string, index: number) => {
-        const targetKey = s3.extractS3Key(url);
-        const newUrl = await s3.moveObject(targetKey, `${uploaderId}/${newKitId}/${index}`);
-
-        return newUrl;
-      }),
-    );
-
-    const newBlurUrl = await s3.moveObject(s3.extractS3Key(blurredImage), `${uploaderId}/${newKitId}/6`);
+    const newKitUrls = await s3.moveToLongTermStorage([...imageUrls, blurredImage], uploaderId, newKitId);
 
     const kit = await prisma.kit.create({
       data: {
@@ -57,9 +85,9 @@ export async function POST(request: Request) {
         stamps: {
           create: newKitUrls.map((url) => ({ image: url })),
         },
-        rewardImage: newKitUrls[5],
-        thumbnailImage: newKitUrls[0],
-        blurredImage: newBlurUrl,
+        rewardImage: newKitUrls[REWARD_IMAGE_INDEX],
+        thumbnailImage: newKitUrls[THUMBNAIL_IMAGE_INDEX],
+        blurredImage: newKitUrls[BLURRED_IMAGE_INDEX],
         tags,
         uploaderId,
       },
--- app/api/s3/presign/route.ts
+++ app/api/s3/presign/route.ts
@@ -1,16 +1,30 @@
 import { NextResponse } from 'next/server';
 import { S3Manager } from '@/lib/services/s3';
+import { auth } from '@/auth';
 
 export async function POST(request: Request) {
-  // TODO: 테스트용 상수 제거
-  const userId = 'clwnjgnp10004dnxeusl9exo3';
+  // TODO: auth, 필수 항목 검증 미들웨어 구현
+  const session = await auth();
+  const currentUser = session?.user;
+
+  if (!currentUser) return NextResponse.json({ error: '로그인 해주세요.' }, { status: 403 });
+
+  const userId = currentUser.id;
+
+  if (!userId) return NextResponse.json({ error: '사용자 ID가 유효하지 않습니다.' }, { status: 403 });
+
   const { fileName } = await request.json();
+
+  if (!fileName) {
+    return NextResponse.json({ error: '파일 이름이 필요합니다.' }, { status: 400 });
+  }
+
   const s3 = new S3Manager();
 
   try {
     const presignedUrl = await s3.getPresignedUrl(fileName, userId);
 
-    return NextResponse.json({ data: presignedUrl });
+    return NextResponse.json({ data: presignedUrl }, { status: 201 });
   } catch (error) {
     return NextResponse.json({ error: '업로드 URL을 생성하지 못했습니다.' }, { status: 500 });
   }
