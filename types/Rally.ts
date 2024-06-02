e6302d3168c444d129034abe4a9b2c31d72390bf
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-02 15:27:53 +0900

    issue/#70 add props in RallyInfo type

--- types/Rally.ts
+++ types/Rally.ts
@@ -1,8 +1,13 @@
-import { RallyModel, UserModel } from './models';
+import { KitModel, RallyModel, UserModel } from './models';
 export { RallyStatus } from './models';
 
-type RallyStarter = { starter: Pick<UserModel, 'image' | 'name'> };
-export type RallyInfo = Pick<RallyModel, 'id' | 'title'> & RallyStarter;
+type RallyStarter = { starter: Pick<UserModel, 'id' | 'image' | 'name'> };
+type RallyKit = {
+  kit: Pick<KitModel, 'id' | 'title' | 'description' | 'tags' | 'thumbnailImage' | 'rewardImage' | 'stamps'>;
+};
+export type RallyInfo = Pick<RallyModel, 'id' | 'title' | 'description' | 'status' | 'stampCount' | 'createdAt' | 'updatedAt'> &
+  RallyStarter &
+  RallyKit;
 
 export interface MyRally extends Pick<RallyModel, 'id' | 'status' | 'stampCount' | 'updatedAt'> {
   kit: Pick<RallyModel['kit'], 'thumbnailImage' | 'title'> & { _count: { stamps: number } };
