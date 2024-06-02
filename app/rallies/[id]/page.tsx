fafbf2ef27d199a7e09a99e45e92420e2b9e8aa5
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-02 15:32:00 +0900

    issue/#70 define rally page

---
+++ app/rallies/[id]/page.tsx
@@ -0,0 +1,10 @@
+interface RallyPageProps {
+  params: { id: string };
+}
+
+export default async function RallyPage({ params: { id } }: RallyPageProps) {
+  return (
+    <main className="px-4 py-6 bg-grey-50 flex flex-col gap-6">
+    </main>
+  );
+}
