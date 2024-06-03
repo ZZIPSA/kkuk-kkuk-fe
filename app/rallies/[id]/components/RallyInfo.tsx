cbf4ac5752e7bf8a0c71b87669a4ad1c634919fa
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-03 17:41:17 +0900

    issue/#70 remove deadline
    https://github.com/ZZIPSA/kkuk-kkuk-fe/pull/95#discussion_r1623999119

--- app/rallies/[id]/components/RallyInfo.tsx
+++ app/rallies/[id]/components/RallyInfo.tsx
@@ -1,5 +1,5 @@
 import { Progress } from '@/components/ui/progress';
-import { convertMsToDate, dateIntl } from '@/lib/date';
+import { /* convertMsToDate, */ dateIntl } from '@/lib/date';
 import { RallyStatus } from '@/types/Rally';
 import { rallyInfoStyles } from './styles';
 
@@ -8,21 +8,21 @@ interface RallyInfoProps {
   percentage: number;
   createdAt: Date;
   updatedAt: Date | null;
-  deadline: Date;
+  // deadline: Date;
   status: RallyStatus;
 }
 
-export default function RallyInfo({ title, percentage, createdAt, updatedAt, deadline, status }: RallyInfoProps) {
-  const today = new Date();
+export default function RallyInfo({ title, percentage, createdAt, updatedAt, /* deadline, */ status }: RallyInfoProps) {
+  // const today = new Date();
   const isActive = status === 'active';
-  const dDay = deadline.getTime() - today.getTime();
+  // const dDay = deadline.getTime() - today.getTime();
 
   return (
     <section className={rallyInfoStyles.container}>
       <h1 className={rallyInfoStyles.title}>{title}</h1>
       <span className={rallyInfoStyles.percentage}>{percentage.toFixed(0)}%</span>
       <Progress value={percentage} className={rallyInfoStyles.progress} />
-      {isActive && <div className={rallyInfoStyles.dDay}>D-day {convertMsToDate(dDay)}</div>}
+      {/* {isActive && <div className={rallyInfoStyles.dDay}>D-day {convertMsToDate(dDay)}</div>} */}
       <p className={rallyInfoStyles.date}>
         <span className={rallyInfoStyles.startDate}>시작일: {dateIntl.format(new Date(createdAt))}</span>
         {!isActive && // TODO: 실패 시에는 표시하지 않아야 함
--- app/rallies/[id]/page.tsx
+++ app/rallies/[id]/page.tsx
@@ -21,16 +21,15 @@ export default async function RallyPage({ params: { id } }: RallyPageProps) {
     createdAt,
     updatedAt,
     kit: { stamps },
-    status, // TODO: 활성화 / 비활성화 보다는 진행 / 완료 / 실패로 나누는게 어떤지 검토
+    status, // TODO: 활성화 / 비활성화 보다는 진행 / 완료 / 실패로 나누는게 어떤지 검토 -> 기한 추가 시 재검토
     // description, // TODO: 상세 설명을 표시 칸 추가
     // starter, // TODO: 로직 중 isStampable 계산 시 사용 예정
   } = rally;
   const total = stamps.length;
   const percentage = (stampCount / total) * 100;
-  // TODO: D-day 계산 방법 << DB 에 종료 예정일(기한) 필드 추가 필요
-  // 임시로 7일 뒤 종료로 설정
-  let deadline = new Date();
-  deadline.setDate(deadline.getDate() + 8);
+  // // TODO: D-day 계산 방법 << DB 에 종료 예정일(기한) 필드 추가 필요 -> 기한 추가 시 재검토
+  // let deadline = new Date();
+  // deadline.setDate(deadline.getDate() + 8);
   const owned = true; // user?.id === starter.id;
   // const isUpdatedToday = false;
   const isStampable = true; // owned && status === 'active' && (isUpdatedToday || stampCount < total);
@@ -38,7 +37,7 @@ export default async function RallyPage({ params: { id } }: RallyPageProps) {
   const stampButtonVariant = getButtonVariant(isStampable, isRewardable);
   return (
     <main className="px-4 py-6 bg-grey-50 flex flex-col gap-6">
-      <RallyInfo title={title} percentage={percentage} createdAt={createdAt} updatedAt={updatedAt} status={status} deadline={deadline} />
+      <RallyInfo title={title} percentage={percentage} createdAt={createdAt} updatedAt={updatedAt} status={status} /* deadline={deadline} */ />
       <RallyStamps stamps={stamps} total={total} stampCount={stampCount} owned={owned} />
       <RallyFooter owned={owned} variant={stampButtonVariant} />
     </main>
