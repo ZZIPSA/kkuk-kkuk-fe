2bfc160ace4ff8bb08e4bb9770a4af138901eaf8
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-03 17:05:19 +0900

    issue/#97 use id as temporary variables

--- app/rallies/[id]/page.tsx
+++ app/rallies/[id]/page.tsx
@@ -17,15 +17,17 @@ export default async function RallyPage({ params: { id } }: RallyPageProps) {
   // const user = await getMember(); // TODO: 로직 중 isStampable 계산 시 사용 예정
   const {
     title,
-    stampCount,
+    // stampCount, TODO: 로직 완료 후 주석 해제
     createdAt,
     updatedAt,
     kit: { stamps },
-    status, // TODO: 활성화 / 비활성화 보다는 진행 / 완료 / 실패로 나누는게 어떤지 검토
+    // status, // TODO: 로직 완료 후 주석 해제 // TODO: 활성화 / 비활성화 보다는 진행 / 완료 / 실패로 나누는게 어떤지 검토
     // description, // TODO: 상세 설명을 표시 칸 추가
     // starter, // TODO: 로직 중 isStampable 계산 시 사용 예정
   } = rally;
   const total = stamps.length;
+  const stampCount = Number(id.at(0)); // TODO: 레이아웃 테스트용 임시 변수로 ID 첫자리를 스탬프 개수로 사용
+  const status = stampCount === total || id.at(3) === '1' ? 'inactive' : 'active'; // TODO: 레이아웃 테스트용 임시 변수로 도장을 모두 채웠거나 ID 넷째자리가 '1'이면 'inactive', '0'이면 'active'
   const percentage = (stampCount / total) * 100;
   // TODO: D-day 계산 방법 << DB 에 종료 예정일(기한) 필드 추가 필요
   // 임시로 7일 뒤 종료로 설정
