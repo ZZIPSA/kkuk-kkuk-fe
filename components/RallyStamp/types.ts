ef749d730f195bc9a82abc97b20d53a02de3d94d
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-04 14:23:03 +0900

    issue/#70 define StampInfo type

--- components/RallyStamp/types.ts
+++ components/RallyStamp/types.ts
@@ -7,3 +7,8 @@ export enum StampKind {
   default = 'default', // 기본 스탬프
   reward = 'reward', // 보상 스탬프
 }
+export interface StampInfo {
+  status: StampStatus;
+  kind: StampKind;
+  owned: boolean;
+}
