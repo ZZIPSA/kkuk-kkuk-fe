faaf253bc2ce9de52345210df055a9b1d8ceb4fc
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-04 14:30:14 +0900

    issue/#70 get conditions of elements

--- components/RallyStamp/index.tsx
+++ components/RallyStamp/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import { Check, Gift } from '@/lib/icons';
 import { cn } from '@/lib/utils';
 import { RallyPreviewStamp } from '@/types/Stamp';
-import { getConditions } from './lib';
+import { getConditions, getElementConditions } from './lib';
 import { StampInfo } from './types';
 
 interface StampProps extends RallyPreviewStamp, StampInfo {
@@ -17,33 +17,8 @@ export function RallyStamp({ image, status, kind, owned, order }: StampProps) {
   const rows = Math.floor(order / STAMP_BY_ROW);
   const isReverse = rows % 2 === 1;
   order += isReverse ? STAMP_BY_ROW - 1 - (order % STAMP_BY_ROW) * 2 : 0;
-  const border = {
-    // 테두리 색상
-    primary: !is.uncheckable && !is.reward, // 체크 가능한 기본 스탬프
-    indigo: !is.uncheckable && is.reward, // 체크 가능한 보상 스탬프
-    grey: is.uncheckable, // 체크 불가능한 스탬프
-    solid: !is.uncheckable,
-    dashed: is.uncheckable,
-  };
-  const filter = {
-    // 필터
-    grayscale: is.uncheckable,
-  };
-  const icon = {
-    // 아이콘
-    check: is.checked && {
-      // 체크된 스탬프
-      primary: is.default,
-      indigo: is.reward,
-    },
-    gift: is.reward &&
-      !(is.owned && is.checked) && {
-        // 보상 스탬프
-        indigo: !is.uncheckable,
-        grey: is.uncheckable,
-      },
-  };
   const is = getConditions({ status, kind, owned });
+  const { border, filter, icon } = getElementConditions(is);
 
   return (
     <div
@@ -83,3 +58,4 @@ export function RallyStamp({ image, status, kind, owned, order }: StampProps) {
     </div>
   );
 }
+export { StampStatus, StampKind } from './types';
