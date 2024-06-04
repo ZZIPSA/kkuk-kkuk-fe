165db7db55fc2688cda7f32f709dcad2e34134f1
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-04 13:40:59 +0900

    issue/#70 modularize logic part
    https://github.com/ZZIPSA/kkuk-kkuk-fe/pull/95#discussion_r1625320848

--- app/rallies/[id]/components/RallyStamps.tsx
+++ app/rallies/[id]/components/RallyStamps.tsx
@@ -1,7 +1,8 @@
-import { RallyStamp, StampStatus, StampKind } from '@/components/RallyStamp';
+import { RallyStamp } from '@/components/RallyStamp';
 import { RallyPreviewStamp } from '@/types/Stamp';
 import { rallyStampsStyles } from './styles';
 import { RallyStampsInfo } from './types';
+import { addStampPropsByIndex } from './lib';
 
 interface RallyStampsProps extends RallyStampsInfo {
   stamps: RallyPreviewStamp[];
@@ -12,17 +13,9 @@ export default function RallyStamps({ stamps, total, stampCount, owned }: RallyS
     <article className={rallyStampsStyles.container}>
       <h2 className={rallyStampsStyles.title}>스탬프 랠리</h2>
       <section className={rallyStampsStyles.stamps}>
-        {stamps
-          .map((e, i) => ({
-            ...e,
-            order: i,
-            status: i < stampCount ? StampStatus.checked : i === stampCount ? StampStatus.checkable : StampStatus.uncheckable,
-            kind: i === total - 1 ? StampKind.reward : StampKind.default,
-            owned,
-          }))
-          .map(({ id, image, status, kind, owned, order }) => (
-            <RallyStamp key={id} id={id} image={image} status={status} kind={kind} owned={owned} order={order} />
-          ))}
+        {stamps.map(addStampPropsByIndex({ owned, stampCount, total })).map(({ id, image, status, kind, owned, order }) => (
+          <RallyStamp key={id} id={id} image={image} status={status} kind={kind} owned={owned} order={order} />
+        ))}
       </section>
     </article>
   );
