4e939891270e405e3f6778996a8842847469194d
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-04 19:00:31 +0900

    issue/#97 define getRallyInfo

--- app/rallies/[id]/page.tsx
+++ app/rallies/[id]/page.tsx
@@ -1,6 +1,6 @@
 // import { notFound } from 'next/navigation';
 // import { getMember } from '@/auth';
-import { dummy, getTempValue } from './lib';
+import { dummy, getRallyInfo, getTempValue } from './lib';
 import RallyInfo from './components/RallyInfo';
 import RallyStamps from './components/RallyStamps';
 import { RallyFooter } from './components/RallyFooter';
@@ -27,16 +27,13 @@ export default async function RallyPage({ params: { id } }: RallyPageProps) {
   } = rally;
   // TODO: 레이아웃 테스트용 임시 변수로 ID 첫자리를 전날까지 찍은 스탬프 개수, 둘째자리를 오늘 스탬프 여부, 셋째자리를 소유 여부로 사용
   const { stampCount, owned, isStampedToday } = getTempValue(id);
-  const total = stamps.length;
-  const count = stampCount + Number(isStampedToday); // 오늘까지 찍은 스탬프 개수
-  const status = count === total ? 'inactive' : 'active'; // TODO: 레이아웃 테스트용 임시 변수로 도장을 모두 채웠다면 'active', 아니면 'inactive'
-  const percentage = (count / total) * 100;
+  const { total, count, status, percentage } = getRallyInfo({ stamps, stampCount, isStampedToday });
 
   return (
     <main className="px-4 py-6 w-full bg-grey-50 flex flex-col gap-6">
       <RallyInfo title={title} percentage={percentage} createdAt={createdAt} updatedAt={updatedAt} status={status} /* deadline={deadline} */ />
-      <RallyStamps stamps={stamps} total={total} stampCount={stampCount} owned={owned} isStampedToday={isStampedToday} />
-      <RallyFooter owned={owned} status={status} stampCount={stampCount} total={total} isStampedToday={isStampedToday} />
+      <RallyStamps owned={owned} stamps={stamps} stampCount={count} total={total} isStampedToday={isStampedToday} />
+      <RallyFooter owned={owned} status={status} stampCount={count} total={total} isStampedToday={isStampedToday} />
     </main>
   );
 }
