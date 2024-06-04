f69e44471e7a29082c7850667a84ad456c768f25
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-04 14:39:39 +0900

    issue/#70 define stamp styles

---
+++ components/RallyStamp/styles.ts
@@ -0,0 +1,22 @@
+export const stampContainerStyles = {
+  default: 'relative aspect-square rounded-full',
+  primary: 'border-primary',
+  indigo: 'border-indigo-500',
+  grey: 'border-grey-200',
+  solid: 'border-solid border-2',
+  dashed: 'border-dashed border',
+};
+export const stampImageStyles = {
+  default: 'rounded-full w-full aspect-square object-cover bg-background',
+  grayscale: 'filter grayscale',
+};
+export const stampCheckIconStyles = {
+  default: 'absolute right-0 bottom-0 size-[26%] p-1 stroke-white stroke-2 rounded-full fill-none',
+  primary: 'bg-primary',
+  indigo: 'bg-indigo-500',
+};
+export const stampGiftIconStyles = {
+  default: 'absolute right-[25%] bottom-[25%] origin-center size-[50%]',
+  indigo: 'fill-indigo-500',
+  grey: 'fill-grey-400',
+};
