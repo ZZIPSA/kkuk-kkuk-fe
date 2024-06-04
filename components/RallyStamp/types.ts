a5465d202d44de15469ba247ad9617cda2307f8b
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-04 14:22:30 +0900

    issue/#70 move enums to types.ts

---
+++ components/RallyStamp/types.ts
@@ -0,0 +1,9 @@
+export enum StampStatus {
+  checked = 'checked', // 체크된 스탬프
+  checkable = 'checkable', // 체크 가능한 스탬프
+  uncheckable = 'uncheckable', // 체크 불가능한 스탬프
+}
+export enum StampKind {
+  default = 'default', // 기본 스탬프
+  reward = 'reward', // 보상 스탬프
+}
