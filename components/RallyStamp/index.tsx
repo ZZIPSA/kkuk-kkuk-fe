fd82becc3ead6d62c2c2fb7951c8411e0c971389
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-04 14:53:08 +0900

    issue/#70 get styles by getStyles

--- components/RallyStamp/index.tsx
+++ components/RallyStamp/index.tsx
@@ -2,8 +2,7 @@ import Image from 'next/image';
 import { Check, Gift } from '@/lib/icons';
 import { cn } from '@/lib/utils';
 import { RallyPreviewStamp } from '@/types/Stamp';
-import { getConditions, getElementConditions } from './lib';
-import { stampContainerStyles, stampImageStyles, stampCheckIconStyles, stampGiftIconStyles } from './styles';
+import { getConditions, getElementConditions, getStyles } from './lib';
 import { StampInfo } from './types';
 
 interface StampProps extends RallyPreviewStamp, StampInfo {
@@ -18,44 +17,14 @@ export function RallyStamp({ image, status, kind, owned, order }: StampProps) {
   const rows = Math.floor(order / STAMP_BY_ROW);
   const isReverse = rows % 2 === 1;
   order += isReverse ? STAMP_BY_ROW - 1 - (order % STAMP_BY_ROW) * 2 : 0;
-  const is = getConditions({ status, kind, owned });
-  const { border, filter, icon } = getElementConditions(is);
+  const is = getElementConditions(getConditions({ status, kind, owned }));
+  const styles = getStyles(is);
 
   return (
-    <div
-      className={cn(stampContainerStyles.default, `order-${order}`, {
-        [stampContainerStyles.primary]: border.primary,
-        [stampContainerStyles.indigo]: border.indigo,
-        [stampContainerStyles.grey]: border.grey,
-        [stampContainerStyles.solid]: border.solid,
-        [stampContainerStyles.dashed]: border.dashed,
-      })}
-    >
-      <Image
-        fill
-        src={image}
-        alt="Stamp"
-        sizes="360"
-        className={cn(stampImageStyles.default, {
-          [stampImageStyles.grayscale]: filter.grayscale,
-        })}
-      />
-      {icon.check && (
-        <Check
-          className={cn(stampCheckIconStyles.default, {
-            [stampCheckIconStyles.primary]: icon.check.primary,
-            [stampCheckIconStyles.indigo]: icon.check.indigo,
-          })}
-        />
-      )}
-      {icon.gift && (
-        <Gift
-          className={cn(stampGiftIconStyles.default, {
-            [stampGiftIconStyles.indigo]: icon.gift.indigo,
-            [stampGiftIconStyles.grey]: icon.gift.grey,
-          })}
-        />
-      )}
+    <div className={cn(styles.container, `order-${order}`)}>
+      <Image fill src={image} alt="Stamp" sizes="360" className={styles.image} />
+      {is.icon.check && <Check className={styles.check} />}
+      {is.icon.gift && <Gift className={styles.gift} />}
     </div>
   );
 }
