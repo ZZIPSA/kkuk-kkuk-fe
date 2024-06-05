70c2769ea131112f76d5c107fad67c33d98da349
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-05 11:45:55 +0900

    issue/#106 define getConditions

--- components/KitCard/lib.ts
+++ components/KitCard/lib.ts
@@ -1,7 +1,13 @@
 import { DEFAULT_KIT_THUMBNAIL, DEFAULT_PROFILE } from '@/lib/constants';
+import { KitCardVariants } from './types';
 
 export const getDefault = ({ thumbnailImage, name, image }: { thumbnailImage?: string | null; name?: string | null; image?: string | null }) => ({
   thumbnail: thumbnailImage ?? DEFAULT_KIT_THUMBNAIL,
   name: name ?? '',
   image: image ?? DEFAULT_PROFILE,
+});
+export const getConditions = (variant: KitCardVariants) => ({
+  vertical: variant === KitCardVariants.vertical,
+  StartPage: variant === KitCardVariants.StartPage,
+  description: variant === KitCardVariants.description,
 });
