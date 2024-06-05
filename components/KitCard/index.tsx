0eedaf5c493f686cfcded3b167817b6bb0df2008
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-05 12:11:59 +0900

    issue/#106 modularize KitCard

--- components/KitCard/index.tsx
+++ components/KitCard/index.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { cn } from '@/lib/utils';
-import { Tag } from '@/stories/Tag';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Card } from '@/components/ui/card';
 import { KitCardInfo } from '@/types/Kit';
-import { Bookmark, Heart } from '@/lib/icons';
 import { getConditions, getDefault, getStyles } from './lib';
 import { KitCardVariants } from './types';
+import KitCardFooter from './KitCardFooter';
+import KitCardContent from './KitCardContent';
+import KitCardHeader from './KitCardHeader';
 
 interface KitCardProps extends KitCardInfo, React.ComponentPropsWithoutRef<typeof Card> {
   /**
@@ -51,36 +49,9 @@ export default function KitCard({
   const styles = getStyles(is);
   return (
     <Card className={styles.container} {...props}>
-      <CardHeader className={styles.header}>
-        <Image src={thumbnail} alt={title} fill className={styles.thumbnail} />
-      </CardHeader>
-      <CardContent className={styles.content}>
-        <CardTitle className={styles.title}>{title}</CardTitle>
-        <div className={styles.tags}>{tags?.map((tag) => <Tag key={tag} label={tag} className={styles.tag} />)}</div>
-        <div className={styles.uploader}>
-          <Avatar className={styles.avatar}>
-            <AvatarImage src={image} alt={name} />
-            <AvatarFallback>{name}</AvatarFallback>
-          </Avatar>
-          <span className={styles.name}>{name}</span>
-        </div>
-        {is.description && (
-          <div className={styles.buttons}>
-            <button className={styles.button}>
-              <Bookmark className={styles.bookmark} />
-            </button>
-            <button className={styles.button}>
-              <Heart className={styles.heart} />
-            </button>
-          </div>
-        )}
-        {is.StartPage && description && <CardDescription className={styles.description}>{description}</CardDescription>}
-      </CardContent>
-      {is.description && description && (
-        <CardFooter className={styles.footer}>
-          <CardDescription className={styles.footerDescription}>{description}</CardDescription>
-        </CardFooter>
-      )}
+      <KitCardHeader thumbnail={thumbnail} title={title} variant={variant} />
+      <KitCardContent title={title} tags={tags} name={name} image={image} variant={variant} description={description} />
+      {is.description && description && <KitCardFooter variant={variant} description={description} />}
     </Card>
   );
 }
