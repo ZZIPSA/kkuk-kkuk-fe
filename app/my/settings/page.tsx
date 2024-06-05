8a9f724a7157db4688c3a09f290d7505ae4b3b71
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-05 17:56:44 +0900

    issue/#110 modularize styles

--- app/my/settings/page.tsx
+++ app/my/settings/page.tsx
@@ -5,10 +5,14 @@ import TeamInfo from './components/TeamInfo';
 
 export default async function SettingsPage() {
   return (
-    <main className="w-full h-[calc(100vh-3rem)] flex flex-col gap-6 bg-grey-50">
+    <main className={styles.main}>
       <UserInfo variant={UserInfoVariant.settings} />
       <UserSettings />
       <TeamInfo />
     </main>
   );
 }
+
+const styles = {
+  main: 'w-full h-[calc(100vh-3rem)] flex flex-col gap-6 bg-grey-50',
+};
