b26878f66bf48c3c722e86577b29789ce6313d68
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-05 18:05:38 +0900

    issue/#112 spread buttons

--- app/my/settings/components/UserSettings/index.tsx
+++ app/my/settings/components/UserSettings/index.tsx
@@ -3,25 +3,14 @@ import { cn } from '@/lib/utils';
 export function UserSettings() {
   return (
     <section className={styles.default}>
-      {items.map((item, index) => (
-        <button key={index} className={cn(styles.button, item.styles)}>
-          {item.label}
-        </button>
-      ))}
+      <button className={styles.button}>로그아웃</button>
+      <button className={cn(styles.button, styles.deleteButton)}>회원탈퇴</button>
     </section>
   );
 }
 
-const items = [
-  {
-    label: '로그아웃',
-  },
-  {
-    label: '회원탈퇴',
-    styles: 'text-red-500',
-  },
-];
 const styles = {
   default: 'flex flex-col w-full bg-grey-50 divide-y',
   button: 'p-4 bg-background w-full text-left',
+  deleteButton: 'text-red-500',
 };
