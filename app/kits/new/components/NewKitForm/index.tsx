908971665e2c725ae5c72b89eab4e589fe7320aa
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-11 21:44:11 +0900

    issue/#43 change NewKitForm

--- app/kits/new/components/NewKitForm/index.tsx
+++ app/kits/new/components/NewKitForm/index.tsx
@@ -5,20 +5,24 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Form } from '@/components/ui/form';
-import { Button } from '@/stories/Button';
+import { PencilPlus } from '@/lib/icons';
 
-import { FormValues } from './types';
+import Description from './Description';
 import { FormSchema } from './schema';
 import Stamps from './Stamps';
-import Title from './Title';
-import Description from './Description';
 import SuccessModal from './SuccessModal';
 import Tags from './Tags';
+import Title from './Title';
+import { FormValues } from './types';
 
 export default function NewKitForm() {
   const [kitId, setKitId] = useState<string>('');
   const isModalOpen = !!kitId;
-  const form = useForm<FormValues>({ resolver: zodResolver(FormSchema) });
+  const form = useForm<FormValues>({
+    resolver: zodResolver(FormSchema),
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+  });
 
   function onSubmit(data: FormValues) {
     // TODO: Handle form submission
@@ -34,7 +38,9 @@ export default function NewKitForm() {
         <Title control={form.control} />
         <Tags control={form.control} />
         <Description control={form.control} />
-        <Button label={'Submit'} type="submit" />
+        <button type="submit" className="w-full py-4 bg-primary text-white rounded-xl">
+          <PencilPlus className="size-6 fill-white inline" /> 키트 만들기
+        </button>
       </form>
 
       {/* TODO: 배포 시 제거 */}
