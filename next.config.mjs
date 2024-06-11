df47ffab08b1ffe6a7b84c0cfd3f5fdef7762a39
Madang Garden <madang@XVX-016RN.local>
2024-06-12 08:57:40 +0900

    hotfix: 이미지 서버 설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -31,6 +31,20 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        // 더미 이미지 서버
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        // 더미 이미지 서버
+        protocol: 'https',
+        hostname: 'fastly.picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   webpack: (config, options) => {
