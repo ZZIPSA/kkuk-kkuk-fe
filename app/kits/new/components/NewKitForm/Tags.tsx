2a768103c4e7c3133532379acb45192996e30188
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-11 21:42:16 +0900

    issue/#43 restruct Tags

--- app/kits/new/components/NewKitForm/Tags.tsx
+++ app/kits/new/components/NewKitForm/Tags.tsx
@@ -1,21 +1,41 @@
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { useFieldArray } from 'react-hook-form';
+import { XIcon } from 'lucide-react';
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { FormFields } from './types';
+import { TagsField, FormFieldProps } from './types';
+import { handleTagsKeyDown } from './lib';
 
-const Tags: FormFields = ({ control }) => (
-  <FormField
-    control={control}
-    name="tags"
-    render={({}) => (
-      <FormItem>
-        <FormLabel>태그</FormLabel>
-        <FormControl>
-          <Input type="text" className="w-full" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    )}
-  />
-);
+export default function Tags({ control }: FormFieldProps) {
+  const field = useFieldArray({ control, name: 'tags' });
+  return (
+    <FormField
+      control={control}
+      name="tags"
+      render={() => (
+        <FormItem>
+          <FormLabel aria-required>태그</FormLabel>
+          <FormControl>
+            <Input type="text" className="w-full" pattern="\S{2,10}" minLength={2} maxLength={10} onKeyDown={handleTagsKeyDown(field)} />
+          </FormControl>
+          <FormDescription className="flex gap-2 w-full overflow-scroll">
+            {field.fields.map(({ name, id }) => (
+              <Tag key={id} id={id} name={name} field={field} />
+            ))}
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
 
-export default Tags;
+function Tag({ id, name, field }: { id: string; name: string; field: TagsField }) {
+  return (
+    <span key={id} className="border border-primary w-fit rounded-full px-2 py-1 align-middle inline-flex break-keep text-nowrap">
+      #{name}
+      <button onClick={() => field.remove(field.fields.findIndex((field) => field.id === id))}>
+        <XIcon className="size-4" />
+      </button>
+    </span>
+  );
+}
