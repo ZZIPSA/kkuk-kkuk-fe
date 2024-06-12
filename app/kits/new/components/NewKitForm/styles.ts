aaf6508f65429d42c622a26db1967e017d647f4a
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-12 12:53:23 +0900

    issue/#43 define tagsStyles

---
+++ app/kits/new/components/NewKitForm/styles.ts
@@ -0,0 +1,6 @@
+export const tagsStyles = {
+  input: 'w-full',
+  description: 'flex gap-2 w-full h-[26px] overflow-scroll',
+  tag: 'border border-primary w-fit rounded-full px-2 py-1 align-middle inline-flex break-keep text-nowrap',
+  delete: 'size-4',
+};
