98d0f2cdb1fd62820007c624f832e6052a6771da
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-12 19:39:45 +0900

    issue/#131 redefine StampsField

--- app/kits/new/components/NewKitForm/types.ts
+++ app/kits/new/components/NewKitForm/types.ts
@@ -1,5 +1,5 @@
 import type { z } from 'zod';
-import type { Control, UseFormRegisterReturn } from 'react-hook-form';
+import type { Control, UseFieldArrayReturn } from 'react-hook-form';
 import type { FormSchema } from './schema';
 
 export type FormValues = z.infer<typeof FormSchema>;
@@ -8,8 +8,7 @@ export interface FormFields {
   ({ control }: { control: Control<z.infer<typeof FormSchema>> }): JSX.Element;
 }
 
-export interface StampsField {
-  ({ control, stampsRef }: { control: Control<z.infer<typeof FormSchema>>; stampsRef: UseFormRegisterReturn<'stamps'> }): JSX.Element;
 export interface FormFieldsProps {
   control: Control<FormValues>;
 }
+export type StampsField = UseFieldArrayReturn<FormValues, 'stamps', 'id'>;
