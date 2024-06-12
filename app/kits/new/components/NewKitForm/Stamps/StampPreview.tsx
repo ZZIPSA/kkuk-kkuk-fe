2aa573462d6e6c7d873d42e650edab1b8d77f4da
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-12 20:28:59 +0900

    issue/#131 add code for test

--- app/kits/new/components/NewKitForm/Stamps/StampPreview.tsx
+++ app/kits/new/components/NewKitForm/Stamps/StampPreview.tsx
@@ -9,11 +9,14 @@ interface StampPreviewProps {
   field: StampsField;
 }
 export default function StampPreview({ index, field }: StampPreviewProps) {
-  const { blob } = field.fields[index];
+  // TODO: 실제 배포시에는 blob 만 이용
+  // 현재는 S3에 올라가는 것을 확인하기 위해 url을 이용
+  const { url /* blob */ } = field.fields[index];
+  const src = url.startsWith('blob:') ? url : '/api/image/' + url.split('/').slice(4).join('/');
 
   return (
     <div className={stampPreviewStyles}>
-      <Image src={blob} className={stampPreviewImageStyles} alt={`${index + 1}번째 스탬프`} fill sizes="360" priority />
+      <Image src={src} className={stampPreviewImageStyles} alt={`${index + 1}번째 스탬프`} fill sizes="360" priority />
       <Trash className={stampTrashIconStyles} onClick={removeButtonHandler} />
     </div>
   );
