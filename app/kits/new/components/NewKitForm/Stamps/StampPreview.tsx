a8683f23929d2a34802f4f6a23b19c62e6d065d3
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-12 19:46:18 +0900

    issue/#131 show blob in StampPreview Image

--- app/kits/new/components/NewKitForm/Stamps/StampPreview.tsx
+++ app/kits/new/components/NewKitForm/Stamps/StampPreview.tsx
@@ -8,10 +8,12 @@ interface StampPreviewProps {
   field: StampsField;
 }
 export default function StampPreview({ index, field }: StampPreviewProps) {
+  const { blob } = field.fields[index];
+
   return (
     <div className={stampPreviewStyles}>
-      <Image src={URL.createObjectURL(file)} className={stampPreviewImageStyles} alt={file.name} fill sizes="360" priority />
       <Trash className={stampTrashIconStyles} onClick={() => setFile(undefined)} />
+      <Image src={blob} className={stampPreviewImageStyles} alt={`${index + 1}번째 스탬프`} fill sizes="360" priority />
     </div>
   );
 }
