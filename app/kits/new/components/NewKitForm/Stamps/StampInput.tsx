8d2546a42d9df771df31b7b62d63d792bd1c6250
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-12 19:59:31 +0900

    issue/#131 apply stampInputHandler

--- app/kits/new/components/NewKitForm/Stamps/StampInput.tsx
+++ app/kits/new/components/NewKitForm/Stamps/StampInput.tsx
@@ -2,9 +2,9 @@
 
 import { useState } from 'react';
 import { StampsField } from '../types';
+import { getStampLabelStyles, getStampSpanStyles, getStampSpanContents, stampInputHandler } from './lib';
 import StampPreview from './StampPreview';
 import EmptyStamp from './EmptyStamp';
-import { getStampLabelStyles, getStampSpanStyles, getStampSpanContents } from './lib';
 
 interface StampInputProps {
   index: number;
@@ -22,7 +22,7 @@ export default function StampInput({ index, total, field }: StampInputProps) {
     <label className={getStampLabelStyles(isFirst, isLast)}>
       <span className={getStampSpanStyles(isFirst, isLast, isEmpty)}>{getStampSpanContents(index, total)}</span>
       {isUploaded ? <StampPreview index={index} field={field} /> : <EmptyStamp index={index} total={total} />}
-      <input type="file" accept="image/*" hidden onChange={(e) => setFile(e.target.files?.[0])} />
+      <input type="file" accept="image/*" hidden onChange={stampInputHandler(field, index)} />
     </label>
   );
 }
