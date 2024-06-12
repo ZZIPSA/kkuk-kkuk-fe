697b446cd572e6747254243da4cb207ebfbba29f
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-12 12:22:54 +0900

    issue/#43 fix tags height

--- app/kits/new/components/NewKitForm/Tags.tsx
+++ app/kits/new/components/NewKitForm/Tags.tsx
@@ -17,7 +17,7 @@ export default function Tags({ control }: FormFieldProps) {
           <FormControl>
             <Input type="text" className="w-full" pattern="\S{2,10}" minLength={2} maxLength={10} onKeyDown={handleTagsKeyDown(field)} />
           </FormControl>
-          <FormDescription className="flex gap-2 w-full overflow-scroll">
+          <FormDescription className="flex gap-2 w-full h-[26px] overflow-scroll">
             {field.fields.map(({ name, id }) => (
               <Tag key={id} id={id} name={name} field={field} />
             ))}
