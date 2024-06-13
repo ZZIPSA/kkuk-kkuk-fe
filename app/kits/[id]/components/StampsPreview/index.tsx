87da066270313160e8414688accbe64fbe5aec85
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-13 17:24:16 +0900

    issue/#138 separate styles

--- app/kits/[id]/components/StampsPreview/index.tsx
+++ app/kits/[id]/components/StampsPreview/index.tsx
@@ -6,23 +6,25 @@ type StampPreviewProps = { stamps: RallyPreviewStamp[] };
 
 export default function StampsPreview({ stamps }: StampPreviewProps) {
   return (
-    <section className="bg-grey-50 px-4 pb-6 flex flex-col gap-4">
-      <h2 className="font-bold">스탬프 목록</h2>
-      <div className="grid grid-cols-3 auto-rows-auto gap-2">
+    <section className={styles.container}>
+      <h2 className={styles.title}>스탬프 목록</h2>
+      <div className={styles.stamps}>
         {stamps.map(({ id, objectKey }, index, { length }) => (
-          <div key={id} className="w-full aspect-square relative">
-            <Image
-              src={encodeURI(`${process.env.API_URL}/api/image/${objectKey}`)}
-              fill
-              alt="Stamp"
-              className="rounded-lg aspect-square object-cover"
-              sizes="240"
-              priority
-            />
-            {index === length - 1 && <Gift className="absolute origin-center size-full p-[25%] fill-grey-400" />}
+          <div key={id} className={styles.stamp}>
+            <Image src={encodeURI(`/api/image/${objectKey}`)} fill alt="Stamp" className={styles.image} sizes="240" priority />
+            {index === length - 1 && <Gift className={styles.gift} />}
           </div>
         ))}
       </div>
     </section>
   );
 }
+
+const styles = {
+  container: 'bg-grey-50 px-4 pb-6 flex flex-col gap-4',
+  title: 'font-bold',
+  stamps: 'grid grid-cols-3 auto-rows-auto gap-2',
+  stamp: 'w-full aspect-square relative',
+  image: 'rounded-lg aspect-square object-cover',
+  gift: 'absolute origin-center size-full p-[25%] fill-grey-400',
+};
