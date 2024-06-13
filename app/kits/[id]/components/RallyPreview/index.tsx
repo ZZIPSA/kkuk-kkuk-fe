7adf951de6ebf39b2ba725bce8a5cbae04097bcb
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-13 17:10:25 +0900

    issue/#138 separte styles

--- app/kits/[id]/components/RallyPreview/index.tsx
+++ app/kits/[id]/components/RallyPreview/index.tsx
@@ -5,10 +5,10 @@ type RallyPreviewProps = { stamps: RallyPreviewStamp[] };
 
 export default function RallyPreview({ stamps }: RallyPreviewProps) {
   return (
-    <section className="bg-grey-50 border-t border-grey-200 px-4 py-6 flex flex-col gap-6">
-      <h2 className="font-bold">스탬프 랠리 미리보기</h2>
-      <div className="bg-white p-6 rounded-2xl">
-        <div className="bg-rally-route bg-contain bg-center bg-no-repeat grid grid-cols-3 auto-rows-auto gap-y-6 gap-x-2">
+    <section className={styles.container}>
+      <h2 className={styles.title}>스탬프 랠리 미리보기</h2>
+      <div className={styles.background}>
+        <div className={styles.stamps}>
           {stamps
             .map((e, i) => ({
               ...e,
@@ -25,3 +25,9 @@ export default function RallyPreview({ stamps }: RallyPreviewProps) {
     </section>
   );
 }
+const styles = {
+  container: 'bg-grey-50 border-t border-grey-200 px-4 py-6 flex flex-col gap-6',
+  title: 'font-bold',
+  background: 'bg-white p-6 rounded-2xl',
+  stamps: 'bg-rally-route bg-contain bg-center bg-no-repeat grid grid-cols-3 auto-rows-auto gap-y-6 gap-x-2',
+};
