caf4f8b972a3cadadb6e2eedea1d05dbd151bc4a
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-13 19:03:41 +0900

    issue/#139 define Submit for NewKitForm

---
+++ app/kits/new/components/NewKitForm/Submit.tsx
@@ -0,0 +1,17 @@
+import type { FormState } from 'react-hook-form';
+import { default as Button } from '@/components/Submit';
+import { PencilPlus } from '@/lib/icons';
+import type { FormValues } from './types';
+
+export default function Submit({ state }: { state: FormState<FormValues> }) {
+  return (
+    <Button className={styles.button} state={state}>
+      <PencilPlus className={styles.pencilPlus} />
+      키트 만들기
+    </Button>
+  );
+}
+const styles = {
+  button: 'bg-primary w-full',
+  pencilPlus: 'fill-white',
+};
