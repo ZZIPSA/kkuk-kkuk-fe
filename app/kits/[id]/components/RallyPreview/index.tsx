ca5226ca5cf9758236f10907e5acf49a0eb8e3da
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-13 17:58:43 +0900

    issue/#136 remove relates with order prop

--- app/kits/[id]/components/RallyPreview/index.tsx
+++ app/kits/[id]/components/RallyPreview/index.tsx
@@ -12,13 +12,12 @@ export default function RallyPreview({ stamps }: RallyPreviewProps) {
           {stamps
             .map((e, i, { length }) => ({
               ...e,
-              order: i,
               status: i === 0 ? StampStatus.checked : i === 1 ? StampStatus.checkable : StampStatus.uncheckable,
               kind: i === length - 1 ? StampKind.reward : StampKind.default,
               owned: false,
             }))
-            .map(({ id, objectKey, status, kind, owned, order }) => (
-              <RallyStamp key={id} id={id} objectKey={objectKey} status={status} kind={kind} owned={owned} order={order} />
+            .map(({ id, objectKey, status, kind, owned }) => (
+              <RallyStamp key={id} id={id} objectKey={objectKey} status={status} kind={kind} owned={owned} />
             ))}
         </div>
       </div>
--- app/rallies/[id]/components/RallyStamps.tsx
+++ app/rallies/[id]/components/RallyStamps.tsx
@@ -13,8 +13,8 @@ export default function RallyStamps({ stamps, total, stampCount, owned, isStampe
     <article className={rallyStampsStyles.container}>
       <h2 className={rallyStampsStyles.title}>스탬프 랠리</h2>
       <section className={rallyStampsStyles.stamps}>
-        {stamps.map(addStampPropsByIndex({ owned, stampCount, total, isStampedToday })).map(({ id, objectKey, status, kind, owned, order }) => (
-          <RallyStamp key={id} id={id} objectKey={objectKey} status={status} kind={kind} owned={owned} order={order} />
+        {stamps.map(addStampPropsByIndex({ owned, stampCount, total, isStampedToday })).map(({ id, objectKey, status, kind, owned }) => (
+          <RallyStamp key={id} id={id} objectKey={objectKey} status={status} kind={kind} owned={owned} />
         ))}
       </section>
     </article>
