412710190c3fba867a30cb3c52dfa8a594993064
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-13 19:13:42 +0900

    issue/#137 show uncompleted message

--- app/my/settings/@modal/(.)delete-account/page.tsx
+++ app/my/settings/@modal/(.)delete-account/page.tsx
@@ -20,11 +20,16 @@ function Backdrop() {
 }
 
 function Description() {
+  // TODO: 기능 완성 후 원상복구 (기존 내용 제거 및 주석 해제)
   return (
     <p className={styles.text}>
-      <h1 className={styles.title}>정말로 탈퇴 하시겠어요? </h1>
+      <h1 className={styles.title}>
+        준비 중인 기능입니다.
+        {/* 정말로 탈퇴 하시겠어요? */}
+      </h1>
       <br />
-      회원 탈퇴시 참여했던 랠리 및<br /> 세트 목록은 복구 불가능해요!
+      관리자에게 문의해주세요.
+      {/* 회원 탈퇴시 참여했던 랠리 및<br /> 세트 목록은 복구 불가능해요! */}
     </p>
   );
 }
@@ -32,11 +37,13 @@ function Description() {
 function Buttons() {
   return (
     <div className={styles.buttons}>
-      {buttons.map(({ key, className, label, href }) => (
-        <Link href={href} key={key} className={className}>
-          {label}
-        </Link>
-      ))}
+      {buttons
+        .filter(({ key }) => key !== 'delete') // TODO: 기능 완성 후 delete 버튼 활성화(해당 줄 제거)
+        .map(({ key, className, label, href }) => (
+          <Link href={href} key={key} className={className}>
+            {label}
+          </Link>
+        ))}
     </div>
   );
 }
