bbe0868b784c51c6767ece4ce5f85805c6878acc
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-16 15:06:00 +0900

    issue/#143 import modules

--- app/api/kits/route.ts
+++ app/api/kits/route.ts
@@ -1,6 +1,9 @@
+import cuid from 'cuid';
 import { NextResponse } from 'next/server';
+import sharp from 'sharp';
 import { auth } from '@/auth';
 import { kitSelect, prisma } from '@/app/api/lib/prisma';
+import { getPresignedUrl, uploadWebp } from '@/app/api/lib/utils';
 import { S3Manager } from '@/lib/services/s3';
 import { BLURRED_IMAGE_INDEX, REWARD_IMAGE_INDEX, THUMBNAIL_IMAGE_INDEX } from '@/app/api/lib/constants';
 import { CreateKitProps } from '@/types/Kit';
