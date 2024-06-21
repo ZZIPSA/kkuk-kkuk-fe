7156f9d328300cb667ec485a425e53b8c439a602
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-21 19:05:17 +0900

    issue/#148 set new kit title

--- app/kits/new/page.tsx
+++ app/kits/new/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import NewKitForm from './components/NewKitForm';
+
+export const metadata: Metadata = {
+  title: '새로운 키트 만들기',
+};
+
 export default async function NewKitPage() {
   return (
     <main className="w-full mx-auto px-4 py-6">
