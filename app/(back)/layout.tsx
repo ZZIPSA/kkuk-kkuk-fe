65cabb345495c75b9c6b223f45f332959579dcb9
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-24 18:58:40 +0900

    Revert "issue/#151 add NavHeader in BackLayout"
    This reverts commit 4bdbc3bd978bd0574ee607b0c609a113130cb874.

--- app/(back)/layout.tsx
+++ app/(back)/layout.tsx
@@ -1,10 +1,8 @@
-import NavHeader from '@/components/NavHeader';
 import BackHeader from '@/components/BackHeader';
 
 export default function BackLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
-      <NavHeader />
       <BackHeader />
       {children}
     </>
