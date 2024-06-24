4bdbc3bd978bd0574ee607b0c609a113130cb874
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-24 17:58:14 +0900

    issue/#151 add NavHeader in BackLayout

--- app/(back)/layout.tsx
+++ app/(back)/layout.tsx
@@ -1,8 +1,10 @@
+import NavHeader from '@/components/NavHeader';
 import BackHeader from '@/components/BackHeader';
 
 export default function BackLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
+      <NavHeader />
       <BackHeader />
       {children}
     </>
