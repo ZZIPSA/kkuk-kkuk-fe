11abecf76fc953a879339601fa9f0feb2c73df06
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-24 14:20:56 +0900

    issue/#151 define getInfo for BackHeader

---
+++ components/BackHeader/lib.ts
@@ -0,0 +1,29 @@
+export const getInfo = ({ path }: { path: string }) => ({
+  title: getTitle(path),
+  back: getBack(path),
+});
+
+const getBack = (path: string) => path.split('/').slice(0, -1).join('/') || '/';
+
+function getTitle(path: string) {
+  switch (path) {
+    // /my/settings
+    case path.match(/\/my\/settings.*/)?.[0]:
+      return '유저 설정';
+    // /kits/new
+    case path.match(/\/kits\/new/)?.[0]:
+      return '키트 업로드';
+    // /kits/[id]/start
+    case path.match(/\/kits\/[^\/]+\/start/)?.[0]:
+      return '랠리 시작하기';
+    // /kits/[id]
+    case path.match(/\/kits\/[^\/]+/)?.[0]:
+      return '스탬프 랠리';
+    // /rallies/[id]
+    case path.match(/\/rallies\/[^\/]+/)?.[0]:
+      return '스탬프 랠리 진행중';
+    // /rallies
+    case path.match(/\/rallies/)?.[0]:
+      return '진행중인 랠리';
+  }
+}
