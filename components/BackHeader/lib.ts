500206c474aba7c740abf9ee9a14050b93b6f26d
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-24 14:53:09 +0900

    issue/#151 replace switch with if

--- components/BackHeader/lib.ts
+++ components/BackHeader/lib.ts
@@ -6,27 +6,18 @@ export const getInfo = ({ path }: { path: string }) => ({
 const getBack = (path: string) => path.split('/').slice(0, -1).join('/') || '/';
 
 function getTitle(path: string) {
-  switch (path) {
-    // /my/settings
-    case path.match(/\/my\/settings.*/)?.[0]:
-      return '유저 설정';
-    // /kits/new
-    case path.match(/\/kits\/new/)?.[0]:
-      return '키트 업로드';
-    // /kits/[id]/start
-    case path.match(/\/kits\/[^\/]+\/start/)?.[0]:
-      return '랠리 시작하기';
-    // /kits/[id]
-    case path.match(/\/kits\/[^\/]+/)?.[0]:
-      return '스탬프 랠리';
-    // /rallies/[id]
-    case path.match(/\/rallies\/[^\/]+/)?.[0]:
-      return '스탬프 랠리 진행중';
-    // /rallies
-    case path.match(/\/rallies/)?.[0]:
-      return '진행중인 랠리';
-    // 경로의 제목이 정의되지 않은 경우
-    default:
-      return '타이틀이 지정되지 않았습니다.';
-  }
+  // /my/settings
+  if (path.match(/\/my\/settings.*/)?.[0]) return '유저 설정';
+  // /kits/new
+  if (path.match(/\/kits\/new/)?.[0]) return '키트 업로드';
+  // /kits/[id]/start
+  if (path.match(/\/kits\/[^\/]+\/start/)?.[0]) return '랠리 시작하기';
+  // /kits/[id]
+  if (path.match(/\/kits\/[^\/]+/)?.[0]) return '스탬프 랠리';
+  // /rallies/[id]
+  if (path.match(/\/rallies\/[^\/]+/)?.[0]) return '스탬프 랠리 진행중';
+  // /rallies
+  if (path.match(/\/rallies/)?.[0]) return '진행중인 랠리';
+  // 경로의 제목이 정의되지 않은 경우
+  return '타이틀이 지정되지 않았습니다.';
 }
