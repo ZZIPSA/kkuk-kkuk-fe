e5612237c8d071f6254b2bf47a33bb8cd5c00ab0
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-24 14:21:21 +0900

    issue/#151 define BackHeader component

---
+++ components/BackHeader/index.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { LeftArrow } from '@/lib/icons';
+import { getInfo } from './lib';
+
+export default function BackHeader() {
+  const { title, back } = getInfo({ path: usePathname() });
+
+  return (
+    <header className="flex justify-center items-center px-4 py-2 w-full top-0 sticky bg-background z-40 font-bold">
+      <Link href={back} className="absolute left-0 p-2">
+        <LeftArrow className="w-6 h-6 fill-foreground/80" />
+      </Link>
+      {title}
+    </header>
+  );
+}
