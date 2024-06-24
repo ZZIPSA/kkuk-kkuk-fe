e3ca40acc484ed9276252ad974f6cf15a0012e0e
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-24 17:37:25 +0900

    issue/#151 fix shadow

--- components/NavHeader/index.tsx
+++ components/NavHeader/index.tsx
@@ -3,7 +3,7 @@ import NavBar from '@/components/NavBar';
 
 export default function NavHeader() {
   return (
-    <header className="border-b px-4 py-2 w-full top-0 sticky bg-background z-40 shadow-lg shadow-foreground/20">
+    <header className="border-b px-4 py-2 w-full top-0 sticky bg-background z-40 shadow-lg shadow-foreground/5">
       <SessionProvider>
         <NavBar />
       </SessionProvider>
