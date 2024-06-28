c37f71312513504c7c7d79a101d4c95668165225
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-06-28 11:30:11 +0900

    issue/#168 apply getRallyInfoDates

--- app/(back)/rallies/[id]/components/RallyInfo.tsx
+++ app/(back)/rallies/[id]/components/RallyInfo.tsx
@@ -1,28 +1,26 @@
 import { Progress } from '@/components/ui/progress';
-import { /* convertMsToDate, */ dateIntl } from '@/lib/date';
 import { RallyData } from '@/types/Rally';
 import { rallyInfoStyles } from './styles';
+import { getRallyInfoDates } from '../lib';
 
 interface RallyInfoProps extends Pick<RallyData, 'title' | 'createdAt' | 'updatedAt' | 'status'> {
   deadline: Date;
   percentage: number;
 }
 
-  // const today = new Date();
-  const isActive = status === 'active';
-  // const dDay = deadline.getTime() - today.getTime();
 export default function RallyInfo({ title, percentage, createdAt, updatedAt, deadline, status }: RallyInfoProps) {
+  const { dDay, since, until, completed } = getRallyInfoDates({ createdAt, updatedAt, deadline, status, percentage });
 
   return (
     <section className={rallyInfoStyles.container}>
       <h1 className={rallyInfoStyles.title}>{title}</h1>
       <span className={rallyInfoStyles.percentage}>{percentage.toFixed(0)}%</span>
       <Progress value={percentage} className={rallyInfoStyles.progress} />
-      {/* {isActive && <div className={rallyInfoStyles.dDay}>D-day {convertMsToDate(dDay)}</div>} */}
+      {dDay !== null && <div className={rallyInfoStyles.dDay}>D-day {dDay}</div>}
       <p className={rallyInfoStyles.date}>
-        <span className={rallyInfoStyles.startDate}>시작일: {dateIntl.format(new Date(createdAt))}</span>
-        {!isActive && // TODO: 실패 시에는 표시하지 않아야 함
-          updatedAt !== null && <span>종료일: {dateIntl.format(new Date(updatedAt))}</span>}
+        <span className={rallyInfoStyles.startDate}>시작일: {since}</span>
+        {until && <span>기한: {until}</span>}
+        {completed && <span>완료일: {completed}</span>}
       </p>
     </section>
   );
