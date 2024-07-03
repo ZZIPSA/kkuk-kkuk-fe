29c4573847dd99776c7abfcf9a329a559cf476fc
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-07-03 22:16:05 +0900

    issue/#168 get percentage from getRallyDates

--- app/(back)/rallies/[id]/components/RallyInfo.tsx
+++ app/(back)/rallies/[id]/components/RallyInfo.tsx
@@ -5,11 +5,10 @@ import { getRallyDates, GetRallyDatesProps } from '../lib';
 
 interface RallyInfoProps extends Pick<RallyData, 'title' | 'createdAt' | 'updatedAt' | 'status'>, GetRallyDatesProps {}
 
-export default function RallyInfo({ title, percentage, createdAt, updatedAt, deadline, status }: RallyInfoProps) {
-  const { dDay, since, completed } = getRallyInfoDates({ createdAt, updatedAt, deadline, status, percentage });
+export default function RallyInfo({ title, createdAt, updatedAt, deadline, status, count, total }: RallyInfoProps) {
+  const { dDay, since, completedAt, percentage } = getRallyDates({ createdAt, updatedAt, deadline, status, count, total });
 
   return (
-        {completed && <span>완료일: {completed}</span>}
     <section className={styles.container}>
       <h1 className={styles.title}>{title}</h1>
       <span className={styles.percentage}>{percentage.toFixed(0)}%</span>
@@ -17,6 +16,7 @@ export default function RallyInfo({ title, percentage, createdAt, updatedAt, dea
       {dDay !== null && <div className={styles.dDay}>D-day {dDay}</div>}
       <p className={styles.date}>
         <span className={styles.startDate}>시작일: {since}</span>
+        {completedAt && <span>완료일: {completedAt}</span>}
       </p>
     </section>
   );
