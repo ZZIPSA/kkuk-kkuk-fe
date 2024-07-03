0a5d15b3117b8f44a20f65c4435502cf00c2f3a2
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-07-03 21:56:41 +0900

    issue/#168 define utils for date

--- lib/date.ts
+++ lib/date.ts
@@ -1,5 +1,9 @@
+import { curry, map, pipe, join } from '@fxts/core';
 import { DATE_TO_MS } from './constants';
 
 const pad02 = (num: number) => num.toString().padStart(2, '0');
-export const displayDateYyMmDd = (date: Date) => [date.getFullYear(), date.getMonth() + 1, date.getDate()].map(pad02).join('.');
+export const displayDateYyMmDd = (date: Date) => pipe(date, getDateNumbers, map(pad02), join('.'));
+export const getDateNumbers = (date: Date) => [date.getFullYear(), date.getMonth() + 1, date.getDate()];
 export const convertMsToDate = (ms: number) => Math.floor(ms / DATE_TO_MS);
+export const now = () => new Date();
+export const diffDates = (date1: Date) => (date2: Date) => Math.abs(date1.getTime() - date2.getTime());
