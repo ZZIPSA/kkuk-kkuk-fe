0b891c7a7948ccfa516260f9fb0bae11906beda7
Madang Garden <chooi9522@gmail.com>
2024-07-06 01:00:28 +0900

    issue/#186 feat: 랠리 연장 API 추가

---
+++ app/api/rallies/[id]/extend/route.ts
@@ -0,0 +1,38 @@
+import { BadRequestError, NotFoundRallyError, ServerError, UnauthorizedError } from '@/app/api/lib/errors';
+import { prisma, rallySelect } from '@/app/api/lib/prisma';
+import { auth } from '@/auth';
+import { NextResponse } from 'next/server';
+type PatchRallyParams = { params: { id: string } };
+
+// NOTE: 랠리 연장 API
+export async function PATCH(request: Request, { params }: PatchRallyParams) {
+  const session = await auth();
+  const currentUser = session?.user;
+
+  const { id: rallyId } = params;
+
+  if (!currentUser) return UnauthorizedError;
+
+  try {
+    const rally = await prisma.rally.findUnique({
+      where: { id: rallyId },
+      include: { kit: { include: { _count: { select: { stamps: true } } } } },
+    });
+
+    if (!rally) return NotFoundRallyError;
+    if (rally.extendedDueDate || !rally.dueDate) return BadRequestError;
+
+    const remainStamp = rally.kit._count.stamps - rally.stampCount;
+    const extendedDueDate = new Date(rally.dueDate);
+
+    extendedDueDate.setDate(extendedDueDate.getDate() + remainStamp);
+
+    const updatedRally = await prisma.rally.update({
+      where: { id: rallyId },
+      data: { extendedDueDate },
+    });
+    return NextResponse.json({ data: updatedRally }, { status: 200 });
+  } catch (error) {
+    return ServerError;
+  }
+}
