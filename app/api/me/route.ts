1f32076001cbd2438375bfdf8a61c27c752da92a
Madang Garden <chooi9522@gmail.com>
2024-07-06 01:59:05 +0900

    issue/#186 feat: 기존 API에 asc/desc 파람 추가

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -1,14 +1,32 @@
 import { NextResponse } from 'next/server';
 import { prisma, kitSelect, rallySelect, userSelect } from '@/app/api/lib/prisma';
+import { SortOrder } from '@/app/api/lib/types';
+import { UnauthorizedError } from '@/app/api/lib/errors';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const userId = searchParams.get('userId');
+  const orderParam = searchParams.get('order') || 'desc';
+  const order: SortOrder = orderParam === 'asc' ? 'asc' : 'desc';
 
-  if (!userId) return NextResponse.json({ error: '로그인 해주세요.' }, { status: 401 });
+  if (!userId) return UnauthorizedError;
 
   const user = await prisma.user.findUnique({
-    select: { rallies: { select: rallySelect }, kits: { select: kitSelect }, ...userSelect },
+    select: {
+      rallies: {
+        orderBy: {
+          createdAt: order,
+        },
+        select: rallySelect,
+      },
+      kits: {
+        orderBy: {
+          createdAt: order,
+        },
+        select: kitSelect,
+      },
+      ...userSelect,
+    },
     where: { id: userId },
   });
 
--- app/api/my/kits/route.ts
+++ app/api/my/kits/route.ts
@@ -1,15 +1,22 @@
 import { NextResponse } from 'next/server';
 import { kitSelect, prisma } from '@/app/api/lib/prisma';
+import { SortOrder } from '@/app/api/lib/types';
+import { UnauthorizedError } from '@/app/api/lib/errors';
 
 // TODO: MVP 이후 페이지네이션 구현
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const userId = searchParams.get('userId');
+  const orderParam = searchParams.get('order') || 'desc';
+  const order: SortOrder = orderParam === 'asc' ? 'asc' : 'desc';
 
-  if (!userId) return NextResponse.json({ error: '로그인 해주세요.' }, { status: 401 });
+  if (!userId) return UnauthorizedError;
 
   const data = await prisma.kit.findMany({
     where: { uploaderId: userId },
+    orderBy: {
+      createdAt: order,
+    },
     select: kitSelect,
   });
 
