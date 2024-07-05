c36689be8b9f889455db223a2e7bd6331dc4ae91
Madang Garden <chooi9522@gmail.com>
2024-07-06 01:03:24 +0900

    issue/#186 fix: 스탬프 찍기 API에 완료 일자가 추가되도록 수정

--- app/api/rallies/[id]/stamp/route.ts
+++ app/api/rallies/[id]/stamp/route.ts
@@ -1,13 +1,17 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/app/api/lib/prisma';
-import { BadRequestError, NotFoundRallyError, ServerError, StampLimitError } from '@/app/api/lib/errors';
+import { BadRequestError, NotFoundRallyError, ServerError, StampLimitError, UnauthorizedError } from '@/app/api/lib/errors';
 import { getRallyStatus } from '@/app/api/lib/utils';
+import { auth } from '@/auth';
 
 type PatchRallyParams = { params: { id: string } };
 
 export async function PATCH(_: Request, { params }: PatchRallyParams) {
+  const session = await auth();
+  const currentUser = session?.user;
   const { id: rallyId } = params;
 
+  if (!currentUser) return UnauthorizedError;
   if (!rallyId) return BadRequestError;
 
   try {
@@ -20,9 +24,15 @@ export async function PATCH(_: Request, { params }: PatchRallyParams) {
     if (rally.stampCount >= rally.kit._count.stamps) return StampLimitError;
 
     const updatedStampCount = rally.stampCount + 1;
+    const isRallyComplete = rally.stampCount === rally.kit._count.stamps;
     const updatedRally = await prisma.rally.update({
       where: { id: rallyId },
-      data: { stampCount: updatedStampCount, status: getRallyStatus(updatedStampCount), lastStampDate: new Date() },
+      data: {
+        stampCount: updatedStampCount,
+        status: getRallyStatus(updatedStampCount),
+        lastStampDate: new Date(),
+        completionDate: isRallyComplete ? new Date() : null,
+      },
     });
 
     return NextResponse.json(updatedRally);
