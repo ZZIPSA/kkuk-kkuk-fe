faca20da151bdef0705f5434576505bdf55e2f53
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-07-08 18:41:46 +0900

    issue/#169 modularize DeadlineModalContent

--- app/(back)/kits/[id]/start/components/RallyStartForm/DeadlineField/index.tsx
+++ app/(back)/kits/[id]/start/components/RallyStartForm/DeadlineField/index.tsx
@@ -1,14 +1,12 @@
+import { XIcon } from 'lucide-react';
 import { useState } from 'react';
 import type { UseFormSetValue } from 'react-hook-form';
 import Modal from '@/components/Modal';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { convertToDeadline, getDeadlineInfo, inputDeadline, presentDeadline } from './lib';
 import { FormFieldProps, FormValues } from '../types';
-import Picker from './Picker';
-import { XIcon } from 'lucide-react';
+import { inputDeadline, presentDeadline } from './lib';
+import DeadlineModalContent from './ModalContent';
 
 interface DeadlineFieldProps extends FormFieldProps {
   setValue: UseFormSetValue<FormValues>;
@@ -16,12 +14,6 @@ interface DeadlineFieldProps extends FormFieldProps {
 
 export default function DeadlineField({ control, setValue }: DeadlineFieldProps) {
   const [open, setOpen] = useState(false);
-  const { today, durings, datesByMonth, months } = getDeadlineInfo();
-  const [pickerValue, setPickerValue] = useState({
-    during: durings[0],
-    month: months[0],
-    date: datesByMonth[months[0]][0],
-  });
 
   return (
     <FormField
@@ -47,29 +39,7 @@ export default function DeadlineField({ control, setValue }: DeadlineFieldProps)
           <FormDescription>
             완주 기한 설정시 난이도가 매우 올라가게 됩니다.
             <Modal open={open}>
-              <Card className="border-none shadow-none flex flex-col gap-6">
-                <CardHeader className="flex flex-col justify-start items-start p-0">
-                  <CardTitle className="text-base">완주 기한을 선택하세요.</CardTitle>
-                  <CardDescription className="text-left">완주 기한까지 완주하지 못하면 랠리를 더 이상 진행할 수 없습니다.</CardDescription>
-                </CardHeader>
-                <CardContent className="p-0">
-                  <Picker value={pickerValue} setValue={setPickerValue} today={today} durings={durings} months={months} datesByMonth={datesByMonth} />
-                </CardContent>
-                <CardFooter className="flex justify-stretch gap-2 p-0">
-                  <Button variant="outline" className="flex-1" onClick={() => setOpen(false)}>
-                    취소하기
-                  </Button>
-                  <Button
-                    className="flex-1"
-                    onClick={() => {
-                      setValue('deadline', convertToDeadline(today)(pickerValue));
-                      setOpen(false);
-                    }}
-                  >
-                    기한설정
-                  </Button>
-                </CardFooter>
-              </Card>
+              <DeadlineModalContent setOpen={setOpen} setValue={setValue} />
             </Modal>
           </FormDescription>
           <FormMessage />
