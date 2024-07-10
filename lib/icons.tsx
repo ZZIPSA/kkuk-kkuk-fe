c12691858824e403fa4b50408054e54c41fd2006
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-07-10 14:30:44 +0900

    issue/#172 load outline-trash icon

--- lib/icons.tsx
+++ lib/icons.tsx
@@ -1,6 +1,7 @@
 export { default as Pencil } from '@/assets/pencil.svg';
 export { default as Stamp } from '@/assets/stamp.svg';
 export { default as Trash } from '@/assets/trash.svg';
+export { default as OutlineTrash } from '@/assets/outline-trash.svg';
 export { default as Heart } from '@/assets/heart.svg';
 export { default as Bookmark } from '@/assets/bookmark.svg';
 export { default as Check } from '@/assets/check.svg';
