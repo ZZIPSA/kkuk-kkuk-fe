521f826c4e5d434602a9993a9b6fd2cba827b9c7
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-07-10 16:01:08 +0900

    issue/#172 apply getTexts

--- app/(back)/rallies/[id]/@modal/(.)delete/page.tsx
+++ app/(back)/rallies/[id]/@modal/(.)delete/page.tsx
@@ -2,13 +2,15 @@ import Modal, { ModalDescription, ModalTitle } from '@/components/ParallelModal'
 import { SadCat } from '@/lib/icons';
 import { RallyPageProps } from '../../types';
 import { deleteRally } from './actions';
+import { getTexts } from './lib';
 
-export default function DeleteRallyModal({ params: { id } }: RallyPageProps) {
+export default async function DeleteRallyModal({ params: { id } }: RallyPageProps) {
+  const { title, description } = await getTexts(id);
   return (
     <Modal back={`/rallies/${id}`} labels={{ submit: '포기하기', cancel: '뒤로가기' }} onSubmit={deleteRally}>
       <SadCat className="size-18 m-auto" />
-      <ModalTitle title="정말 랠리를 포기하시겠어요?" />
-      <ModalDescription text="랠리를 포기하면 삭제되어 다신 이어갈 수 없어요!" />
+      <ModalTitle title={title} />
+      <ModalDescription description={description} />
     </Modal>
   );
 }
