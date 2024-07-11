114c04562dbe33c861a7d616ec1e1ca3f55f3e9e
Madang Garden <chooi9522@gmail.com>
2024-07-12 04:54:44 +0900

    refactor: 리워드 조건문 수정

--- app/(back)/rallies/[id]/components/RallyStamps.tsx
+++ app/(back)/rallies/[id]/components/RallyStamps.tsx
@@ -1,4 +1,4 @@
-import { RallyStamp } from '@/components/RallyStamp';
+import { RallyStamp, StampKind } from '@/components/RallyStamp';
 import { RallyPreviewStamp } from '@/types/Stamp';
 import { rallyStampsStyles } from './styles';
 import { RallyStampsInfo } from './types';
@@ -16,7 +16,7 @@ export default function RallyStamps({ stamps, total, count, owned, stampable, re
       <h2 className={rallyStampsStyles.title}>스탬프 랠리</h2>
       <section className={rallyStampsStyles.stamps}>
         {stamps.map(addStampPropsByIndex({ owned, count, total, stampable })).map(({ id, objectKey, status, kind, owned }, index) => {
-          if (index === total - 1 && completionDate) objectKey = rewardImage;
+          if (kind === StampKind.reward && completionDate) objectKey = rewardImage;
           return <RallyStamp key={id} id={id} objectKey={objectKey} status={status} kind={kind} owned={owned} />;
         })}
       </section>
