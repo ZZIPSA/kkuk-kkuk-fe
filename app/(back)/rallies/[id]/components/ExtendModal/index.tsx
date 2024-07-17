70d86fe0cf569fc4697f23ef3c8a9a637461e90d
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-07-17 14:44:37 +0900

    issue/#176 apply updates

--- app/(back)/rallies/[id]/components/ExtendModal/index.tsx
+++ app/(back)/rallies/[id]/components/ExtendModal/index.tsx
@@ -12,18 +12,13 @@ interface ExtendModalProps {
 }
 
 export default async function ExtendModal({ id, kitId, extendable, startable }: ExtendModalProps) {
+  const labels = getLabels({ extendable, startable });
   return (
-    <Modal back={`/kit/${kitId}`} labels={{ submit: '기간 연장하기', cancel: '새로 시작하기' }} onSubmit={extendRally}>
+    <Modal back="/rallies" cancel={startable ? `/kits/${kitId}` : '/rallies'} labels={labels} onSubmit={extendRally} className="px-2">
       <SadCat className="size-18 m-auto" />
       <input type="hidden" name="id" value={id} />
       <ModalTitle>아이쿠 아쉬워요!</ModalTitle>
-      <ModalDescription>
-        기간안에 스탬프 랠리를 마무리 하지 못했어요!
-        <br />
-        1회 한정 남은 기간을 연장하여
-        <br />
-        랠리를 이어갈 수 있어요!
-      </ModalDescription>
+      {extendable ? <ExtendableDescription /> : <NotExtendableDescription />}
     </Modal>
   );
 }
