73ffccc1f99af61ff9a5221b1e21bdd73a8cc196
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-07-17 18:34:37 +0900

    issue/#176 modularize ExtendModal

--- app/(back)/rallies/[id]/components/ExtendModal/index.tsx
+++ app/(back)/rallies/[id]/components/ExtendModal/index.tsx
@@ -1,53 +1,23 @@
 import Modal, { ModalDescription, ModalTitle } from '@/components/ParallelModal';
 import { SadCat } from '@/lib/icons';
 import { extendRally } from '../../actions';
-import { pipe } from '@fxts/core';
-import { bind, remain } from '@/lib/do';
+import { getExtendModalInfo } from './lib';
 
-interface ExtendModalProps extends Flags {
+interface ExtendModalProps {
   id: string;
   kitId: string;
-}
-interface Flags {
   extendable: boolean;
   startable: boolean;
 }
 
 export default async function ExtendModal({ id, kitId, extendable, startable }: ExtendModalProps) {
-  const labels = getLabels({ extendable, startable });
+  const { description, labels, cancel } = getExtendModalInfo({ kitId, extendable, startable });
   return (
-    <Modal back="/rallies" cancel={startable ? `/kits/${kitId}` : '/rallies'} labels={labels} onSubmit={extendRally} className="px-2">
+    <Modal back="/rallies" cancel={cancel} labels={labels} onSubmit={extendRally} className="px-4">
       <SadCat className="size-18 m-auto" />
       <input type="hidden" name="id" value={id} />
       <ModalTitle>아이쿠 아쉬워요!</ModalTitle>
-      {extendable ? <ExtendableDescription /> : <NotExtendableDescription />}
+      <ModalDescription className="whitespace-pre">{description}</ModalDescription>
     </Modal>
-  );
-}
-
-const getLabels = (e: Flags) => pipe(e, bind('submit', getSubmit), bind('cancel', getCancel), remain(['submit', 'cancel'] as const));
-const getSubmit = ({ extendable, startable }: Flags) => (extendable ? '기간 연장하기' : startable ? '같은 랠리로 새로 시작하기' : undefined);
-const getCancel = ({ extendable, startable }: Flags) => (extendable && startable ? '새로 시작하기' : undefined);
-
-function ExtendableDescription() {
-  return (
-    <ModalDescription>
-      기간안에 스탬프 랠리를 마무리 하지 못했어요!
-      <br />
-      1회 한정 남은 기간을 연장하여
-      <br />
-      랠리를 이어갈 수 있어요!
-    </ModalDescription>
-  );
-}
-function NotExtendableDescription() {
-  return (
-    <ModalDescription>
-      기간안에 스탬프 랠리를 마무리 하지 못했어요!
-      <br />
-      아쉽지만 이미 연장한 랠리는 또다시 연장할 수 없어요.
-      <br />
-      다시 한 번 도전해보는 건 어떨까요?
-    </ModalDescription>
   );
 }
