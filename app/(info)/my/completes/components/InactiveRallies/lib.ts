16ba5c19437b39f207d49189dd1f58c33d0b640a
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-07-31 14:48:35 +0900

    issue/173 remove unused code

--- app/(info)/my/completes/components/InactiveRallies/lib.ts
+++ app/(info)/my/completes/components/InactiveRallies/lib.ts
@@ -2,7 +2,7 @@ import { bind } from '@/lib/do';
 import { MyRally } from '@/types/Rally';
 import { pipe } from '@fxts/core';
 
-export const getInfo = (rally: MyRally) => pipe(rally, bind('completed', isCompleted), bind('label', getLabel));
+export const getInfo = (rally: MyRally) => pipe(rally, bind('completed', isCompleted));
 
 const isCompleted = ({
   stampCount,
@@ -10,4 +10,3 @@ const isCompleted = ({
     stamps: { length },
   },
 }: MyRally) => stampCount === length;
-const getLabel = ({ completed }: { completed: boolean } & MyRally) => (completed ? '완료' : '실패');
