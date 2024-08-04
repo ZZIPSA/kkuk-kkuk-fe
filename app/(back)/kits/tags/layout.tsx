021d54da711204d953a8ae4d2d57b82367c3f711
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-04 19:56:03 +0900

    issue/#197 define layout for tags page

---
+++ app/(back)/kits/tags/layout.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useCount, useTags } from './hooks';
+
+export default function TagsLayout({ children }: { children: React.ReactNode }) {
+  const tags = useTags();
+  return (
+    <main>
+      <Title tags={tags} />
+      {children}
+    </main>
+  );
+}
+
+function Title({ tags }: { tags: string[] }) {
+  const count = useCount(tags);
+
+  return (
+    <h1 className={styles.title.default}>
+      <span className="">{tags.map<React.ReactNode>((tag) => <Span key={tag}>'#{tag}'</Span>).reduce((prev, curr) => [prev, ', ', curr])}</span>{' '}
+      검색된 키트 <Span>{count}</Span>개
+    </h1>
+  );
+}
+const Span = ({ children }: { children: React.ReactNode }) => <span className={styles.title.primary}>{children}</span>;
+
+const styles = {
+  title: {
+    default: 'font-bold',
+    primary: 'text-primary',
+  },
+};
