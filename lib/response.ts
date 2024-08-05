5603241a40b9b2b045bc1b9504c833fdbe12bfcd
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-05 11:29:44 +0900

    issue/#197 define resolveData util

--- lib/response.ts
+++ lib/response.ts
@@ -4,3 +4,4 @@ const isOk = (res: Response) => res.ok;
 export const validResponse = lift(isOk);
 export const resolveText = (res: Response) => res.text();
 export const resolveJson = <T>(res: Response): Promise<T> => res.json();
+export const resolveData = <T>(res: Response) => res.json().then((data) => data as T);
