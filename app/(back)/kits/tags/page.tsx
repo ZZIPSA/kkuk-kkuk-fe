d5d1f40f4705a508e9b02ce981dfe53ad1a941a0
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-05 17:16:57 +0900

    issue/#197 comment at tags page

--- app/(back)/kits/tags/page.tsx
+++ app/(back)/kits/tags/page.tsx
@@ -8,6 +8,9 @@ import { useTags } from './hooks';
 export default function TagsPage() {
   const tags = useTags();
   const params = tags.map((tag) => `tag=${tag}`).join('&');
+  // NOTE - 일차적으로 태그가 없을 경우 에러를 던지도록 설정
+  // TODO - 태그 페이지에 대한 논의: 태그가 없을 경우에 대한 처리 방법
+  // ex) 태그가 없을 경우 태그를 인기순으로 보여주는 건 어떨지?
   if (!tags) throw new Error('tag is required');
   /** {@link GET API} */
   const api = `/api/kits/tags?${params}`;
