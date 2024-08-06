4a867926a8ed2159c78acc985bf95b518012dcf4
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-06 13:42:50 +0900

    issue/#212 define getUserApi util

---
+++ lib/api.ts
@@ -0,0 +1,5 @@
+import type { GET as GET_USER_API } from '@/app/api/users/[id]/route';
+import { API_URL } from '@/lib/constants';
+
+/** {@link GET_USER_API GET USER API} */
+export const getUserApi = (id: string) => `${API_URL}/api/users/${id}`;
