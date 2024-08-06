e3af5616e05f7d9f9c73b782141707a213819fea
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-06 13:44:15 +0900

    issue/#212 give id from layout

--- app/(info)/my/layout.tsx
+++ app/(info)/my/layout.tsx
@@ -1,10 +1,13 @@
+import { ensureMember } from '@/auth';
 import UserInfo from '@/components/UserInfo';
 import MenuTabs from './components/MenuTabs';
 
 export default async function MyLayout({ children }: { children: React.ReactNode }) {
+  const { id } = await ensureMember();
+
   return (
     <main className="w-full">
-      <UserInfo />
+      <UserInfo id={id} />
       <MenuTabs />
       {children}
     </main>
