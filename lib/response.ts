72a96139a6330320c064e2829cbb3347152936b9
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-06 18:13:20 +0900

    issue/#212 fix resolveJson/Data params type

--- lib/response.ts
+++ lib/response.ts
@@ -3,5 +3,5 @@ import { lift } from './either';
 const isOk = (res: Response) => res.ok;
 export const validResponse = lift(isOk);
 export const resolveText = (res: Response) => res.text();
-export const resolveJson = <T>(res: Response): Promise<T> => res.json();
-export const resolveData = <T>(res: Response) => res.json().then(({ data }) => data as T);
+export const resolveJson = <T>(res: Response | Request): Promise<T> => res.json();
+export const resolveData = <T>(res: Response | Request) => res.json().then(({ data }) => data as T);
