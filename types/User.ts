0346f00a6bbfb841e871eca7ae84a08a935aa6b8
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-06 13:34:12 +0900

    issue/#212 define types for user data

--- types/User.ts
+++ types/User.ts
@@ -1,6 +1,14 @@
+import { accountSelect, Prisma, userInfoSelect } from '@/app/api/lib/prisma';
 import { UserModel, AccountModel, RallyModel } from './models';
 
 export type UserInfoResult = Pick<UserModel, 'image' | 'name'> & {
   accounts: Pick<AccountModel, 'provider' | 'userId'>[];
   rallies: Pick<RallyModel, 'status'>[];
 };
+
+export type UserData = Prisma.UserGetPayload<{
+  where: { id: string };
+  select: typeof userInfoSelect;
+}>;
+
+export type AccountData = Prisma.AccountGetPayload<{ where: { userId: string }; select: typeof accountSelect }>;
