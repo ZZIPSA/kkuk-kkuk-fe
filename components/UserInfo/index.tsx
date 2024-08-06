71eae4d9fe953d0ee90b9c9e71c8490732e58e15
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-06 13:54:01 +0900

    issue/#212 modularize DefaultInfo

--- components/UserInfo/index.tsx
+++ components/UserInfo/index.tsx
@@ -1,19 +1,19 @@
 import { cva, type VariantProps } from 'class-variance-authority';
 import { pipe } from '@fxts/core';
+import { getUserApi } from '@/lib/api';
 import { resolveData } from '@/lib/response';
 import { UserData } from '@/types/User';
-import { ProfileImage, ProfileEditButton } from './ProfileImage';
-import RalliesCounts from './RalliesCounts';
-import { UserInfoVariant } from './variants';
+import DefaultInfo from './DefaultInfo';
 import NicknameInput from './NicknameInput';
-import { getUserApi } from '@/lib/api';
+import { ProfileImage, ProfileEditButton } from './ProfileImage';
+import { UserInfoVariant } from './variants';
 
 interface UserInfoProps extends VariantProps<typeof userInfoVariants> {
   id: string;
 }
 
 const variant = {
-  [UserInfoVariant.default]: 'gap-y-4',
+  [UserInfoVariant.default]: '',
   [UserInfoVariant.settings]: 'grid-rows-[1_auto] justify-items-center gap-4',
 };
 
@@ -24,29 +24,13 @@ const userInfoVariants = cva('flex flex-col items-center py-6 px-4 gap-4 bg-back
 
 export default async function UserInfo({ id, variant = UserInfoVariant.default }: UserInfoProps) {
   const { image, name, accounts, rallies } = await pipe(id, getUserApi, fetch, resolveData<UserData>);
-  // const twitterAccount = accounts.find(({ provider }) => provider === 'twitter');
   return (
     <section className={userInfoVariants({ variant })}>
       <span className="row-span-2 relative">
         <ProfileImage image={image} name={name} variant={variant} />
         {variant === UserInfoVariant.settings && <ProfileEditButton />}
       </span>
-      {variant === UserInfoVariant.default && (
-        <>
-          <h1 className="font-bold text-xl">{name}</h1>
-          {/* NOTE: 페이즈 1에서 구현 
-            {twitterAccount && (
-              <Badge variant="secondary" className="w-fit text-xs font-normal gap-2">
-                <span className="text-2xl">𝕏</span>@{twitterAccount.providerAccountId}
-              </Badge>
-            )}
-          */}
-          {/* NOTE: DB에 User.description이 추가되면 구현
-            <p className="text-sm">{description}</p>
-          */}
-          <RalliesCounts rallies={rallies ?? []} />
-        </>
-      )}
+      {variant === UserInfoVariant.default && <DefaultInfo name={name} rallies={rallies ?? []} />}
       {/* // TODO: name의 nullable을 허용하지 않도록 수정 */}
       {variant === UserInfoVariant.settings && <NicknameInput name={name ?? ''} />}
     </section>
