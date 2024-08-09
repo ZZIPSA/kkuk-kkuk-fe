17bd88d1fda5b78edcb325a78b9b9d0a2721199e
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-09 17:42:08 +0900

    issue/#212 apply ActiveRallies

--- app/(info)/my/joins/page.tsx
+++ app/(info)/my/joins/page.tsx
@@ -1,9 +1,7 @@
 import { Metadata } from 'next';
-import Link from 'next/link';
 import { ensureMember } from '@/auth';
-import RallyCard from '@/components/RallyCard';
 import { fetchUserActiveRallies } from '@/lib/users';
-import { RallyStatus } from '@/types/Rally';
+import ActiveRallies from '@/components/ActiveRallies/page';
 import EmptyContent from '@/components/EmptyContent';
 
 export const metadata: Metadata = {
@@ -15,15 +13,5 @@ export default async function JoinsPage() {
   const rallies = await fetchUserActiveRallies(userId);
   if (rallies.length === 0) return <EmptyContent message="진행중인 랠리가 없어요!" />;
 
-  return (
-    <article className="px-4 py-6 grid grid-cols-2 gap-x-2 gap-y-4">
-      {rallies
-        .filter(({ status }) => status === RallyStatus.active)
-        .map(({ id, stampCount, title, kit: { thumbnailImage, stamps } }) => (
-          <Link key={id} href={`/rallies/${id}`}>
-            <RallyCard stampCount={stampCount} thumbnailImage={thumbnailImage} title={title} stamps={stamps} />
-          </Link>
-        ))}
-    </article>
-  );
+  return <ActiveRallies rallies={rallies} />;
 }
