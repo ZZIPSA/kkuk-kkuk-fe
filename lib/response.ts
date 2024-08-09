e3868856582de310ef73276f2cc55de935beeed3
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-09 16:55:09 +0900

    issue/#212 define utils for fetch

--- lib/response.ts
+++ lib/response.ts
@@ -1,7 +1,13 @@
-import { lift } from './either';
+import { notFound } from 'next/navigation';
+import { lift, purify } from './either';
 
 const isOk = (res: Response) => res.ok;
 export const validResponse = lift(isOk);
-export const resolveText = (res: Response) => res.text();
+export const validOrNotFound = (res: Response) => purify<Response, Response>(notFound)(lift(isOk)(res));
+export const resolveText = (res: Response | Request) => res.text();
 export const resolveJson = <T>(res: Response | Request): Promise<T> => res.json();
 export const resolveData = <T>(res: Response | Request) => res.json().then(({ data }) => data as T);
+export const fetchDataOrNotFound = <T>(...props: Parameters<typeof fetch>) =>
+  fetch(...props)
+    .then(validOrNotFound)
+    .then(resolveData<T>);
