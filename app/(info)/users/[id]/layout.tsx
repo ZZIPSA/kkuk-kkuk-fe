a9c1ffee0a021d29c59b246c133629c8de1c40d3
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-09 16:59:02 +0900

    issue/#212 define /users/[id] layout

---
+++ app/(info)/users/[id]/layout.tsx
@@ -0,0 +1,12 @@
+import UserInfo from '@/components/UserInfo';
+import MenuTabs from './components/MenuTabs';
+
+export default async function UsersLayout({ children, params: { id } }: { children: React.ReactNode; params: { id: string } }) {
+  return (
+    <main className="w-full">
+      <UserInfo id={id} />
+      <MenuTabs id={id} />
+      {children}
+    </main>
+  );
+}
