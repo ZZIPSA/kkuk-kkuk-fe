2ccbb3fddaafb8a5f35295de2e6317f1b3afeb26
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-09 17:41:49 +0900

    issue/#212 apply UploadKits

--- app/(info)/my/uploads/page.tsx
+++ app/(info)/my/uploads/page.tsx
@@ -1,9 +1,8 @@
 import { Metadata } from 'next';
-import Link from 'next/link';
 import { ensureMember } from '@/auth';
-import KitCard from '@/components/KitCard';
 import { fetchUserKits } from '@/lib/users';
 import EmptyContent from '@/components/EmptyContent';
+import UploadKits from '@/components/UploadKits/page';
 
 export const metadata: Metadata = {
   title: '업로드한 키트',
@@ -14,13 +13,5 @@ export default async function UploadsPage() {
   const kits = await fetchUserKits(userId);
   if (kits.length === 0) return <EmptyContent message="업로드한 키트가 없어요!" />;
 
-  return (
-    <article className="px-4 py-6 grid grid-cols-2 gap-x-2 gap-y-4">
-      {kits.map(({ id, tags, thumbnailImage, title, uploader }) => (
-        <Link key={id} href={`/kits/${id}`}>
-          <KitCard id={id} tags={tags} thumbnailImage={thumbnailImage} title={title} uploader={uploader} />
-        </Link>
-      ))}
-    </article>
-  );
+  return <UploadKits kits={kits} />;
 }
