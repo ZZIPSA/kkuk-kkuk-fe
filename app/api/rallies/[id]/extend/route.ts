a2604068d2a28d1c8a36f697a8b516c4a525c56a
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-17 19:03:09 +0900

    issue/#215 fix server logic

--- app/api/rallies/[id]/extend/route.ts
+++ app/api/rallies/[id]/extend/route.ts
@@ -1,17 +1,15 @@
 import { BadRequestError, NotFoundRallyError, ServerError, UnauthorizedError } from '@/app/api/lib/errors';
-import { prisma, rallySelect } from '@/app/api/lib/prisma';
-import { auth } from '@/auth';
+import { prisma } from '@/app/api/lib/prisma';
+import { RallyStatus } from '@prisma/client';
 import { NextResponse } from 'next/server';
 type PatchRallyParams = { params: { id: string } };
 
 // NOTE: 랠리 연장 API
 export async function PATCH(request: Request, { params }: PatchRallyParams) {
-  const session = await auth();
-  const currentUser = session?.user;
-
   const { id: rallyId } = params;
+  const { userId } = await request.json();
 
-  if (!currentUser) return UnauthorizedError;
+  if (!userId) return UnauthorizedError;
 
   try {
     const rally = await prisma.rally.findUnique({
@@ -20,16 +18,17 @@ export async function PATCH(request: Request, { params }: PatchRallyParams) {
     });
 
     if (!rally) return NotFoundRallyError;
+    if (userId !== rally.starterId) return UnauthorizedError;
     if (rally.extendedDueDate || !rally.dueDate) return BadRequestError;
 
     const remainStamp = rally.kit._count.stamps - rally.stampCount;
-    const extendedDueDate = new Date(rally.dueDate);
+    const extendedDueDate = new Date();
 
     extendedDueDate.setDate(extendedDueDate.getDate() + remainStamp);
 
     const updatedRally = await prisma.rally.update({
       where: { id: rallyId },
-      data: { extendedDueDate },
+      data: { extendedDueDate, status: RallyStatus.active },
     });
     return NextResponse.json({ data: updatedRally }, { status: 200 });
   } catch (error) {
