3a9859d2e0bc75e3b7c9e2fcaed98b4a21504cb5
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-19 17:02:03 +0900

    issue/#218 fetch to delete rally

--- app/(back)/rallies/[id]/@modal/(.)delete/actions.ts
+++ app/(back)/rallies/[id]/@modal/(.)delete/actions.ts
@@ -1,11 +1,12 @@
 'use server';
 
+import { redirect } from 'next/navigation';
 import { ensureMember } from '@/auth';
+import { API_URL } from '@/lib/constants';
 
 export const deleteRally = async (form: FormData) => {
   const userId = (await ensureMember()).id;
   const rallyId = form.get('id') as string;
-
-  console.log(`Deleting rally ${rallyId} for user ${userId}`);
-  console.log(`TODO: Implement rally deletion`);
+  fetch(`${API_URL}/api/rallies/${rallyId}`, { method: 'DELETE', body: JSON.stringify({ userId }) });
+  redirect(`/rallies`);
 };
