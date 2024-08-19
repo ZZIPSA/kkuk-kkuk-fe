68faeb99f132239639244b8addc080fbe24862a1
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-19 19:55:18 +0900

    issue/#218 simpllize fetchDataOrNotFound

--- lib/response.ts
+++ lib/response.ts
@@ -7,7 +7,7 @@ export const validOrNotFound = (res: Response) => purify<Response, Response>(not
 export const resolveText = (res: Response | Request) => res.text();
 export const resolveJson = <T>(res: Response | Request): Promise<T> => res.json();
 export const resolveData = <T>(res: Response | Request) => res.json().then(({ data }) => data as T);
-export const fetchDataOrNotFound = <T>(...props: Parameters<typeof fetch>) =>
+export const fetchDataOrNotFound = <T>(...props: Parameters<typeof fetch>): Promise<T> =>
   fetch(...props)
-    .then(validOrNotFound)
-    .then(resolveData<T>);
+    .then(resolveData<T>)
+    .catch(notFound);
