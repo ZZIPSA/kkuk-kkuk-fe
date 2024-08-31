f9c0dbb41c0291e1237ef5637597787a91ffe156
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-31 16:59:41 +0900

    issue/#178 apply fixed getKitData

--- app/(back)/kits/[id]/start/page.tsx
+++ app/(back)/kits/[id]/start/page.tsx
@@ -4,6 +4,7 @@ import KitCard, { KitCardVariants } from '@/components/KitCard';
 import RallyStartForm from './components/RallyStartForm';
 import { getKitData } from '../lib';
 import { KitPageInfo } from '../types';
+import { notFound } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: { absolute: `꾹꾹 | 랠리 시작하기` },
@@ -11,20 +12,11 @@ export const metadata: Metadata = {
 
 export default async function RallyStartPage({ params: { id } }: KitPageInfo) {
   const { id: userId } = await ensureMember();
-  const { data: kit } = await getKitData(id);
-  const { title, description, tags, thumbnailImage, uploader } = kit;
+  const { description, ...kit } = (await getKitData(id)) ?? notFound();
 
   return (
     <main className="w-full py-6 px-4 flex flex-col gap-6">
-      <KitCard
-        variant={KitCardVariants.StartPage}
-        id={id}
-        title={title}
-        description={description || ''}
-        tags={tags}
-        thumbnailImage={thumbnailImage}
-        uploader={uploader}
-      />
+      <KitCard {...kit} description={description || ''} variant={KitCardVariants.StartPage} />
       <RallyStartForm starterId={userId} kitId={id} />
     </main>
   );
